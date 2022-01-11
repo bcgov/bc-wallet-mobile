fd945745c310c3132be2708fd7dadbf6d4b1f779
Jason C. Leach <jason.leach@fullboar.ca>
2022-01-11 14:00:18 -0800

    Shore up release creation
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- scripts/gpublish/index.js
+++ scripts/gpublish/index.js
@@ -1,10 +1,17 @@
 #!/usr/bin/env node
 
+// This script will upload to a Google Play release track. See
+// the Google Developer API documentation reference here:
+// https://developers.google.com/android-publisher/api-ref/rest
+
 const { google } = require('googleapis');
 const fs = require('fs');
+const pjson = require('./package.json');
 
 if (typeof process.env.GOOGLE_API_CREDENTIALS === 'undefined') {
   console.log(`
+  Google Publish v${pjson.version}
+
   GOOGLE_API_CREDENTIALS cannot be empty
 
   Set the env var GOOGLE_API_CREDENTIALS to the full path of the
@@ -16,6 +23,8 @@ if (typeof process.env.GOOGLE_API_CREDENTIALS === 'undefined') {
 
 if (typeof process.env.ANDROID_PACKAGE_NAME === 'undefined') {
   console.log(`
+  Google Publish v${pjson.version}
+
   ANDROID_PACKAGE_NAME cannot be empty
 
   Set the env var ANDROID_PACKAGE_NAME to the full package name
@@ -27,6 +36,8 @@ if (typeof process.env.ANDROID_PACKAGE_NAME === 'undefined') {
 
 if (typeof process.env.ANDROID_BUNDLE_PATH === 'undefined') {
   console.log(`
+  Google Publish v${pjson.version}
+
   ANDROID_BUNDLE_PATH cannot be empty
 
   Set the env var ANDROID_BUNDLE_PATH to the full path to the
@@ -47,12 +58,16 @@ const main = async () => {
   const packageName = process.env.ANDROID_PACKAGE_NAME;
   const bundlePath = process.env.ANDROID_BUNDLE_PATH;
   
+  console.log(`Google Publish v${pjson.version}`);
+
   try {
+    console.log('Creating Google API client.');
     const client = await google.auth.getClient({
       keyFile,
       scopes,
     });
 
+    console.log('Preparing Android publisher.');
     const play = await google.androidpublisher({
       version: 'v3',
       auth: client,
@@ -61,6 +76,7 @@ const main = async () => {
       }
     });
 
+    console.log('Creating an Edit.');
     const edit = await play.edits.insert({
       resource: {
           id: `${new Date().getTime()}`,
@@ -68,8 +84,10 @@ const main = async () => {
       }
     });
 
+    console.log('Loading bundle data.');
     const bundle = fs.readFileSync(bundlePath);
     
+    console.log('Uploading bundle data to Edit.');
     await play.edits.bundles.upload({
       editId: edit.data.id,
       packageName,
@@ -77,6 +95,34 @@ const main = async () => {
           mimeType: 'application/octet-stream',
           body: bundle
       }
+    });
+
+    console.log('Updating internal track.');
+    await play.edits.tracks.update({
+      editId: edit.data.id,
+      packageName,
+      track: 'internal',
+      requestBody: {
+        releases: [{
+          name: `1.0.0-alpha Build ${process.env.GITHUB_RUN_NUMBER}`,
+          status: 'draft', // draft, inProgress, completed
+          // userFraction: 0.99,
+          versionCodes: [
+            `${process.env.GITHUB_RUN_NUMBER ? process.env.GITHUB_RUN_NUMBER : 0}`
+          ],
+          releaseNotes: [
+            {
+              language: 'en-CA',
+              text: "This is what to test."
+            }
+          ],
+        }],
+      },
+    });
+
+    console.log('Committing Edit.');
+    await play.edits.commit({
+      editId: edit.data.id
     });
 
     process.exit(0);
