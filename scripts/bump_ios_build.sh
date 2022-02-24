fe823643c3574273e03bbd4bf4145fcc8bae4262
Jason C. Leach <jason.leach@fullboar.ca>
2022-02-24 15:23:37 -0800

    Fix ios version usage
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- scripts/bump_ios_build.sh
+++ scripts/bump_ios_build.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 set -xaou pipefail
 
-VERSION_STRING="1.0.1"
-
 # /usr/libexec/PlistBuddy \
 # -c "Set :CFBundleVersion ${GITHUB_RUN_NUMBER}" "$1"
 
 # /usr/libexec/PlistBuddy \
 # -c "Set :CFBundleShortVersionString ${VERSION_STRING}" "$1"
 
-agvtool new-version ${GITHUB_RUN_NUMBER}
-agvtool new-marketing-version ${VERSION_STRING}
+agvtool new-version ${CURRENT_PROJECT_VERSION}
+agvtool new-marketing-version ${MARKETING_VERSION}
