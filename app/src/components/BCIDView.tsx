9b83664fcbf29c331d55a420ee76bd71d9bf2550
Jason C. Leach <jason.leach@fullboar.ca>
2022-07-31 12:07:49 -0700

    Add support for foundation credential
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

---
+++ app/src/components/BCIDView.tsx
@@ -0,0 +1,148 @@
+import { ProofState, CredentialState } from "@aries-framework/core";
+import {
+  useAgent,
+  useCredentialByState,
+  useConnections,
+  useProofById,
+  useProofByState,
+  useConnectionById,
+} from "@aries-framework/react-hooks";
+import React, { useEffect } from "react";
+import { View } from "react-native";
+import { useTranslation } from "react-i18next";
+import {
+  Button,
+  ButtonType,
+  testIdWithKey,
+  HomeContentView,
+} from "aries-bifold";
+import { IDIM_PORTAL_URL, IDIM_AGENT_INVITE_URL } from "../constants";
+import { useNavigation } from "@react-navigation/core";
+import { Screens } from "aries-bifold";
+import url from "url";
+
+const trustedInvitationIssueRe =
+  /3Lbd5wSSSBv1xtjwsQ36sj:[0-9]{1,1}:CL:[0-9]{5,}:default/i;
+const trustedFoundationCredentialIssuerRe =
+  /XpgeQa93eZvGSZBZef3PHn:[0-9]{1,1}:CL:[0-9]{5,}:BC\sPerson\sCredential\s\(Dev\)/i;
+
+const BCIDView: React.FC = () => {
+  const [showGetFoundationCredential, setShowGetFoundationCredential] =
+    React.useState<boolean>(false);
+  const credentials = [
+    ...useCredentialByState(CredentialState.CredentialReceived),
+    ...useCredentialByState(CredentialState.Done),
+  ];
+  const { agent } = useAgent();
+  const { t } = useTranslation();
+  const connections = useConnections();
+
+  const [iDIMAgentConnectionId, setIDIMAgentConnectionId] =
+    React.useState<string>();
+  const [coolProofID, setCoolProofID] = React.useState<string>();
+  const receivedProofs = useProofByState(ProofState.RequestReceived);
+  const proof = useProofById(coolProofID);
+  const connection = useConnectionById(proof?.connectionId);
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    for (const p of receivedProofs) {
+      if (
+        p.state == ProofState.RequestReceived &&
+        p.connectionId === iDIMAgentConnectionId
+      ) {
+        setCoolProofID(p.id);
+      }
+    }
+  }, [receivedProofs]);
+
+  useEffect(() => {
+    if (!proof) {
+      return;
+    }
+
+    if (proof.state == ProofState.RequestReceived) {
+      navigation.getParent()?.navigate("Notifications Stack", {
+        screen: Screens.ProofRequest,
+        params: { proofId: proof.id },
+      });
+    }
+
+    if (proof.state == ProofState.Done && connection) {
+      const destUrl = url.resolve(IDIM_PORTAL_URL, connection?.did);
+      navigation.navigate(Screens.WebDisplay, { destUrl });
+    }
+  }, [proof, connection]);
+
+  useEffect(() => {
+    for (const c of connections.connections) {
+      console.log(c);
+    }
+  }, [connections]);
+
+  useEffect(() => {
+    for (const cred of credentials) {
+      const credentialDefinitionId = cred.metadata.data[
+        "_internal/indyCredential"
+      ].credentialDefinitionId as string;
+
+      // If we have a foundation credential, hide the button get get said credential.
+      if (
+        credentialDefinitionId &&
+        trustedFoundationCredentialIssuerRe.test(credentialDefinitionId)
+      ) {
+        setShowGetFoundationCredential(false);
+
+        return;
+      }
+
+      // If we have a foundation invite credential, show the button get get said credential.
+      if (
+        credentialDefinitionId &&
+        trustedInvitationIssueRe.test(credentialDefinitionId)
+      ) {
+        setShowGetFoundationCredential(true);
+
+        return;
+      }
+    }
+  }, [credentials]);
+
+  const onGetIdTouched = async () => {
+    try {
+      const connectionRecord =
+        await agent?.connections.receiveInvitationFromUrl(
+          IDIM_AGENT_INVITE_URL,
+          {
+            autoAcceptConnection: true,
+          }
+        );
+
+      if (!connectionRecord?.id) {
+        throw new Error("Connection does not have an ID");
+      }
+
+      setIDIMAgentConnectionId(connectionRecord.id);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <HomeContentView>
+      {showGetFoundationCredential && (
+        <View style={{ marginVertical: 40, marginHorizontal: 25 }}>
+          <Button
+            title={"Get your BC Digital ID"}
+            accessibilityLabel={"Get BCID"}
+            testID={testIdWithKey("GetBCID")}
+            onPress={onGetIdTouched}
+            buttonType={ButtonType.Secondary}
+          />
+        </View>
+      )}
+    </HomeContentView>
+  );
+};
+
+export default BCIDView;
