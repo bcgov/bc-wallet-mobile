61b7f43535e2b4ea24774656510894749a294a7e
wadeking98 <wkingnumber2@gmail.com>
2022-08-03 12:43:51 -0700

    test
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- app/App.tsx
+++ app/App.tsx
@@ -27,6 +27,7 @@ const App = () => {
   useEffect(() => {
     // Hide the native splash / loading screen so that our
     // RN version can be displayed.
+    // test1
     SplashScreen.hide();
   }, []);
 
