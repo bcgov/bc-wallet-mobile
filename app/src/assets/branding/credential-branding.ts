a4d7ac114bc456c138280e6f2c74c8582f5b2b98
Akiff Manji <akiff.manji@gmail.com>
2022-08-25 10:41:49 -0700

    Update credential branding
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

--- app/src/assets/branding/credential-branding.ts
+++ app/src/assets/branding/credential-branding.ts
@@ -1,22 +1,31 @@
 import { Overlay } from 'aries-bifold/App/types/overlay'
 
+const memberCardOverlay = {
+  imageSource: require('./lsbc-member-card.png'),
+  header: {
+    imageSource: require('./lsbc-header-logo.png'),
+    backgroundColor: '#00698C',
+    hideIssuer: true,
+  },
+}
+
+const studentCardOverlay = {
+  imageSource: require('./best-bc-student-card.png'),
+  header: {
+    imageSource: require('./best-bc-header-logo.png'),
+    color: '#FFFFFF',
+  },
+  footer: { color: '#FFFFFF' },
+}
+
+const idCardOverlay = {}
+
 const branding: Record<string, Overlay> = {
-  'AuJrigKQGRLJajKAebTgWu:2:Member Card:1.5.1': {
-    imageSource: require('./lsbc-member-card.png'),
-    header: {
-      imageSource: require('./lsbc-header-logo.png'),
-      backgroundColor: '#00698C',
-      hideIssuer: true,
-    },
-  },
-  'XUxBrVSALWHLeycAUhrNr9:2:student_card:1.0': {
-    imageSource: require('./best-bc-student-card.png'),
-    header: {
-      imageSource: require('./best-bc-header-logo.png'),
-      color: '#FFFFFF',
-    },
-    footer: { color: '#FFFFFF' },
-  },
+  'AuJrigKQGRLJajKAebTgWu:2:Member Card:1.5.1': memberCardOverlay,
+  '4xE68b6S5VRFrKMMG1U95M:2:Member Card:1.5.1': memberCardOverlay,
+  'Trx3R1frdEzbn34Sp1jyX:2:student_card:1.0': studentCardOverlay,
+  '63ZiwyeZeazA6AhYRYm2zD:2:student_card:1.0': studentCardOverlay,
+  'XUxBrVSALWHLeycAUhrNr9:2:student_card:1.0': studentCardOverlay,
 }
 
 export default branding
