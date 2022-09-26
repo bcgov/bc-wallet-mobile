abeab21b3a0011fafeb835f9c6e3086ef2e2b7a9
Akiff Manji <akiff.manji@gmail.com>
2022-09-26 15:25:09 -0700

    Initialization refactor and style formatting/cleanup
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,13 +1,15 @@
 import { translationResources, ConfigurationContext } from 'aries-bifold'
 import _merge from 'lodash.merge'
-import { defaultTheme as theme } from './theme'
+
+import UseBiometry from '../../bifold/core/App/screens/UseBiometry'
+
+import branding from './assets/branding/credential-branding'
+import BCIDView from './components/BCIDView'
 import en from './localization/en'
 import { pages } from './screens/OnboardingPages'
-import Terms from './screens/Terms'
 import Splash from './screens/Splash'
-import BCIDView from './components/BCIDView'
-import branding from './assets/branding/credential-branding'
-import UseBiometry from '../../bifold/core/App/screens/UseBiometry'
+import Terms from './screens/Terms'
+import { defaultTheme as theme } from './theme'
 
 const localization = _merge({}, translationResources, {
   en: { translation: en },
