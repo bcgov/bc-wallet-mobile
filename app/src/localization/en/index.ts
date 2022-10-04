092d75208ead10f26cb4b68658c1c24ba061fda3
Akiff Manji <akiff.manji@gmail.com>
2022-10-04 14:35:32 -0700

    Updates biometrics wording
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

--- app/src/localization/en/index.ts
+++ app/src/localization/en/index.ts
@@ -36,9 +36,9 @@ const translation = {
   },
   "Biometry": {
     "Toggle": "Toggle Biometrics",
-    "EnabledText1": "To keep your information secure,",
+    "EnabledText1": "Log in with your phone's biometrics instead of your wallet PIN.",
     "EnabledText1Bold": "you will need to use biometrics to open your BC Wallet.",
-    "EnabledText2": "BC Wallet does not record or save any biometric data.",
+    "EnabledText2": "This means all fingerprint and facial data added on this phone can be used to access your BC Wallet.",
     "EnabledText3": "Anyone who can access your phone with biometrics can access your BC Wallet.",
     "EnabledText3Bold": "Ensure only you have access to your wallet.",
     "Warning": "Ensure only you have access to your wallet.",
