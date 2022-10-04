f9ea34f4c004baf85c39a9d5e203a501dfc641a4
Akiff Manji <akiff.manji@gmail.com>
2022-10-04 09:52:40 -0700

    chore: update styling to support a11y enabled device features
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

--- app/src/screens/OnboardingPages.tsx
+++ app/src/screens/OnboardingPages.tsx
@@ -1,13 +1,12 @@
+import { Button, ButtonType, Theme, createStyles, GenericFn, testIdWithKey } from 'aries-bifold'
 import React from 'react'
 import { Text, View } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
 import { SvgProps } from 'react-native-svg'
 
 import CredentialList from '../assets/img/credential-list.svg'
 import ScanShare from '../assets/img/scan-share.svg'
 import SecureImage from '../assets/img/secure-image.svg'
-import { Button, ButtonType, Theme, createStyles } from 'aries-bifold'
-import { GenericFn } from 'aries-bifold'
-import { testIdWithKey } from 'aries-bifold'
 
 const endPage = (onTutorialCompleted: GenericFn, theme: Theme['OnboardingTheme']) => {
   const defaultStyle = createStyles(theme)
@@ -18,38 +17,26 @@ const endPage = (onTutorialCompleted: GenericFn, theme: Theme['OnboardingTheme']
   }
   return (
     <>
-      <View style={{ alignItems: 'center' }}>
-        <SecureImage {...imageDisplayOptions} />
-      </View>
-      <View style={{ marginLeft: 20, marginRight: 20, marginTop: 30 }}>
-        <Text style={[defaultStyle.headerText, { fontSize: 18 }]}>Privacy and confidentiality</Text>
-        <Text style={[defaultStyle.bodyText, { marginTop: 20 }]}>
-          You approve every use of information from your BC Wallet. You also only share what is needed for a situation.
-          {'\n\n'}
-          The Government of British Columbia is not told when you use your digital credentials.
-        </Text>
-      </View>
+      <ScrollView style={{ padding: 20 }}>
+        <View style={{ alignItems: 'center' }}>
+          <SecureImage {...imageDisplayOptions} />
+        </View>
+        <View style={{ marginBottom: 20 }}>
+          <Text style={[defaultStyle.headerText, { fontSize: 18 }]}>Privacy and confidentiality</Text>
+          <Text style={[defaultStyle.bodyText, { marginTop: 25 }]}>
+            You approve every use of information from your BC Wallet. You also only share what is needed for a
+            situation.
+            {'\n\n'}
+            The Government of British Columbia is not told when you use your digital credentials.
+          </Text>
+        </View>
+      </ScrollView>
       <View
         style={{
           marginTop: 'auto',
-          marginBottom: 45,
-          marginLeft: 20,
-          marginRight: 20,
+          margin: 20,
         }}
       >
-        {/* <View style={[defaultStyle.point, { marginTop: 60 }]}>
-          <Icon name={'info'} size={30} color={Colors.text} style={{ marginRight: 5 }} />
-          <TouchableOpacity
-            accessibilityLabel={'Learn More'}
-            accessible
-            onPress={() => Linking.openURL('https://example.com/')}
-          >
-            <Text style={[defaultStyle.bodyText, { color: 'blue', textDecorationLine: 'underline' }]}>
-              Learn more about the BC Wallet
-            </Text>
-          </TouchableOpacity>
-          <Icon name={'open-in-new'} size={14} color={Colors.text} style={{ marginLeft: 5 }} />
-        </View> */}
         <Button
           title={'Get Started'}
           accessibilityLabel={'Get Started'}
@@ -65,19 +52,19 @@ const endPage = (onTutorialCompleted: GenericFn, theme: Theme['OnboardingTheme']
 const startPages = (theme: Theme) => {
   const defaultStyle = createStyles(theme)
   return (
-    <>
-      <Text style={[defaultStyle.headerText, { marginLeft: 20, marginRight: 20 }]}>Welcome</Text>
-      <Text style={[defaultStyle.bodyText, { marginLeft: 20, marginTop: 35, marginRight: 20 }]}>
+    <ScrollView style={{ padding: 20 }}>
+      <Text style={[defaultStyle.headerText]}>Welcome</Text>
+      <Text style={[defaultStyle.bodyText, { marginTop: 25 }]}>
         BC Wallet lets you receive, store and use digital credentials.
       </Text>
-      <Text style={[defaultStyle.bodyText, { marginLeft: 20, marginTop: 25, marginRight: 20 }]}>
+      <Text style={[defaultStyle.bodyText, { marginTop: 25 }]}>
         It is highly secure, and helps protect your privacy online.
       </Text>
-      <Text style={[defaultStyle.bodyText, { marginLeft: 20, marginTop: 25, marginRight: 20 }]}>
+      <Text style={[defaultStyle.bodyText, { marginTop: 25 }]}>
         BC Wallet is currently in its early stages and the technology is being explored. Most people will not have a use
         for BC Wallet yet, because very few digital credentials are available.
       </Text>
-    </>
+    </ScrollView>
   )
 }
 
@@ -106,13 +93,13 @@ const createPageWith = (image: React.FC<SvgProps>, title: string, body: string, 
     width: 180,
   }
   return (
-    <>
+    <ScrollView style={{ padding: 20 }}>
       <View style={{ alignItems: 'center' }}>{image(imageDisplayOptions)}</View>
-      <View style={{ marginLeft: 20, marginRight: 20, marginTop: 30 }}>
+      <View style={{ marginBottom: 20 }}>
         <Text style={[defaultStyle.headerText, { fontSize: 18 }]}>{title}</Text>
-        <Text style={[defaultStyle.bodyText, { marginTop: 20 }]}>{body}</Text>
+        <Text style={[defaultStyle.bodyText, { marginTop: 25 }]}>{body}</Text>
       </View>
-    </>
+    </ScrollView>
   )
 }
 
