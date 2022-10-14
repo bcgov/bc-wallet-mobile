63bec8cbbf1fca78e5a918d559c07cff47e60142
Clécio Varjão <1348549+cvarjao@users.noreply.github.com>
2022-10-14 08:27:04 -0700

    feat: OCA support, update subodule (#646)
    * Fix: OCA support, update submodule
    
    Signed-off-by: Clécio Varjão <1348549+cvarjao@users.noreply.github.com>

--- app/src/assets/branding/credential-branding.ts
+++ app/src/assets/branding/credential-branding.ts
@@ -1,5 +1,3 @@
-import { Overlay } from 'aries-bifold/App/types/overlay'
-
 const demoMemberCardOverlay = {
   imageSource: require('./lsbc-member-card-demo.png'),
   header: {
@@ -62,24 +60,172 @@ const digitalIdCardOverlay = {
   footer: { color: '#FFFFFF' },
 }
 
-const branding: Record<string, Overlay> = {
-  // ↓↓↓ https://github.com/bcgov/bc-wallet-mobile/discussions/370
-  'AuJrigKQGRLJajKAebTgWu:2:Member Card:1.5.1': demoMemberCardOverlay /* LSBC (TEST) */,
-  '4xE68b6S5VRFrKMMG1U95M:2:Member Card:1.5.1': memberCardOverlay /* LSBC (PROD) */,
-  // ↓↓↓ https://github.com/bcgov/BC-Wallet-Demo/discussions/59
-  'Trx3R1frdEzbn34Sp1jyX:2:Member Card:1.5.1': demoMemberCardOverlay /* Showcase LSBC (DEV) */,
-  '63ZiwyeZeazA6AhYRYm2zD:2:Member Card:1.5.1': demoMemberCardOverlay /* Showcase LSBC (TEST) */,
-  'XUxBrVSALWHLeycAUhrNr9:2:Member Card:1.5.1': demoMemberCardOverlay /* Showcase LSBC (PROD) */,
-  'Trx3R1frdEzbn34Sp1jyX:2:student_card:1.0': studentCardOverlay /* Showcase Student (DEV) */,
-  '63ZiwyeZeazA6AhYRYm2zD:2:student_card:1.0': studentCardOverlay /* Showcase Student (TEST) */,
-  'XUxBrVSALWHLeycAUhrNr9:2:student_card:1.0': studentCardOverlay /* Showcase Student (PROD) */,
-  // ↓↓↓ https://github.com/bcgov/bc-wallet-mobile/discussions/506
-  'Ui6HA36FvN83cEtmYYHxrn:2:unverified_person:0.1.0': idCardOverlay /* Unverified Person (DEV) */,
-  'XZQpyaFa9hBUdJXfKHUvVg:2:unverified_person:0.1.0': idCardOverlay /* Unverified Person (TEST) */,
-  '9wVuYYDEDtpZ6CYMqSiWop:2:unverified_person:0.1.0': idCardOverlay /* Unverified Person (DEV) */,
-  // ↓↓↓ https://github.com/bcgov/bc-wallet-mobile/discussions/604
-  '3Lbd5wSSSBv1xtjwsQ36sj:2:BC VC Pilot Certificate:1.0.1': digitalIdInvitationCardOverlay /* (TEST) */,
-  'XpgeQa93eZvGSZBZef3PHn:2:Person:0.1': digitalIdCardOverlay /* (TEST) */,
+const idCardBundle = {
+  capture_base: {},
+  overlays: [
+    {
+      type: 'spec/overlays/meta/1.0',
+      language: 'en',
+      name: 'Person',
+      issuer_name: 'Service BC',
+    },
+    {
+      type: 'spec/overlays/meta/1.0',
+      language: 'fr',
+      name: 'Personne',
+      issuer_name: 'Service BC',
+    },
+    {
+      type: 'spec/overlays/card_layout/1.0',
+      ...idCardOverlay,
+    },
+    {
+      type: 'spec/overlays/label/1.0',
+      language: 'en',
+      attr_labels: {
+        given_names: 'Given Name',
+        family_name: 'Family Name',
+      },
+    },
+    {
+      type: 'spec/overlays/label/1.0',
+      language: 'fr',
+      attr_labels: {
+        given_names: 'Prénoms',
+        family_name: 'Nom de famille',
+      },
+    },
+  ],
 }
 
-export default branding
+const digitalIdCardBundle = {
+  capture_base: {},
+  overlays: [
+    {
+      type: 'spec/overlays/meta/1.0',
+      language: 'en',
+      name: 'Person',
+      issuer_name: 'Service BC',
+    },
+    {
+      type: 'spec/overlays/meta/1.0',
+      language: 'fr',
+      name: 'Personne',
+      issuer_name: 'Service BC',
+    },
+    {
+      type: 'spec/overlays/card_layout/1.0',
+      ...digitalIdCardOverlay,
+    },
+    {
+      type: 'spec/overlays/label/1.0',
+      language: 'en',
+      attr_labels: {
+        given_names: 'Given Name',
+        family_name: 'Family Name',
+      },
+    },
+    {
+      type: 'spec/overlays/label/1.0',
+      language: 'fr',
+      attr_labels: {
+        given_names: 'Prénoms',
+        family_name: 'Nom de famille',
+      },
+    },
+  ],
+}
+
+const demoMemberCardBundle = {
+  capture_base: {},
+  overlays: [
+    {
+      type: 'spec/overlays/meta/1.0',
+      language: 'en',
+      name: 'Person',
+      issuer_name: 'Law Society of BC',
+    },
+    {
+      type: 'spec/overlays/card_layout/1.0',
+      ...demoMemberCardOverlay,
+    },
+    {
+      type: 'spec/overlays/label/1.0',
+      language: 'en',
+      attr_labels: {
+        given_names: 'Given Name',
+        family_name: 'Family Name',
+      },
+    },
+  ],
+}
+
+const memberCardBundle = {
+  capture_base: {},
+  overlays: [
+    {
+      type: 'spec/overlays/meta/1.0',
+      language: 'en',
+      name: 'Member',
+      issuer_name: 'Law Society of BC',
+    },
+    {
+      type: 'spec/overlays/card_layout/1.0',
+      ...memberCardOverlay,
+    },
+  ],
+}
+
+const studentCardBundle = {
+  capture_base: {},
+  overlays: [
+    {
+      type: 'spec/overlays/meta/1.0',
+      language: 'en',
+      name: 'Student',
+      issuer_name: 'BestBC College',
+    },
+    {
+      type: 'spec/overlays/card_layout/1.0',
+      ...studentCardOverlay,
+    },
+  ],
+}
+
+const digitalIdInvitationCardBundle = {
+  capture_base: {},
+  overlays: [
+    {
+      type: 'spec/overlays/meta/1.0',
+      language: 'en',
+      name: 'Pilot Invitation',
+      issuer_name: 'DITP',
+    },
+    {
+      type: 'spec/overlays/card_layout/1.0',
+      ...digitalIdInvitationCardOverlay,
+    },
+  ],
+}
+
+export default {
+  // ↓↓↓ https://github.com/bcgov/bc-wallet-mobile/discussions/370
+  /* LSBC (TEST) */
+  'AuJrigKQGRLJajKAebTgWu:2:Member Card:1.5.1': demoMemberCardBundle,
+  '4xE68b6S5VRFrKMMG1U95M:2:Member Card:1.5.1': memberCardBundle /* LSBC (PROD) */,
+  // ↓↓↓ https://github.com/bcgov/BC-Wallet-Demo/discussions/59
+  'Trx3R1frdEzbn34Sp1jyX:2:Member Card:1.5.1': demoMemberCardBundle /* Showcase LSBC (DEV) */,
+  '63ZiwyeZeazA6AhYRYm2zD:2:Member Card:1.5.1': demoMemberCardBundle /* Showcase LSBC (TEST) */,
+  'XUxBrVSALWHLeycAUhrNr9:2:Member Card:1.5.1': demoMemberCardBundle /* Showcase LSBC (PROD) */,
+  'Trx3R1frdEzbn34Sp1jyX:2:student_card:1.0': studentCardBundle /* Showcase Student (DEV) */,
+  '63ZiwyeZeazA6AhYRYm2zD:2:student_card:1.0': studentCardBundle /* Showcase Student (TEST) */,
+  /* Showcase Student (PROD) */
+  'XUxBrVSALWHLeycAUhrNr9:2:student_card:1.0': studentCardBundle,
+  // ↓↓↓ https://github.com/bcgov/bc-wallet-mobile/discussions/506
+  'Ui6HA36FvN83cEtmYYHxrn:2:unverified_person:0.1.0': idCardBundle /* Unverified Person (DEV) */,
+  'XZQpyaFa9hBUdJXfKHUvVg:2:unverified_person:0.1.0': idCardBundle /* Unverified Person (TEST) */,
+  '9wVuYYDEDtpZ6CYMqSiWop:2:unverified_person:0.1.0': idCardBundle /* Unverified Person (DEV) */,
+  // ↓↓↓ https://github.com/bcgov/bc-wallet-mobile/discussions/604
+  '3Lbd5wSSSBv1xtjwsQ36sj:2:BC VC Pilot Certificate:1.0.1': digitalIdInvitationCardBundle /* (TEST) */,
+  'XpgeQa93eZvGSZBZef3PHn:2:Person:0.1': digitalIdCardBundle /* (TEST) */,
+}
--- app/src/index.ts
+++ app/src/index.ts
@@ -1,16 +1,16 @@
-import { translationResources, ConfigurationContext } from 'aries-bifold'
+import { translationResources, ConfigurationContext, types, Record } from 'aries-bifold'
+import { Bundles } from 'aries-bifold/lib/typescript/App/types/oca'
 import _merge from 'lodash.merge'
 
 import UseBiometry from '../../bifold/core/App/screens/UseBiometry'
 
-import branding from './assets/branding/credential-branding'
+import bundles from './assets/branding/credential-branding'
 import BCIDView from './components/BCIDView'
 import en from './localization/en'
 import { pages } from './screens/OnboardingPages'
 import Splash from './screens/Splash'
 import Terms from './screens/Terms'
 import { defaultTheme as theme } from './theme'
-
 const localization = _merge({}, translationResources, {
   en: { translation: en },
 })
@@ -19,8 +19,9 @@ const configuration: ConfigurationContext = {
   splash: Splash,
   terms: Terms,
   homeContentView: BCIDView,
-  OCABundle: branding,
+  OCABundle: new types.oca.DefaultOCABundleResolver().loadBundles(bundles as unknown as Bundles),
   useBiometry: UseBiometry,
+  record: Record,
 }
 
 export default { theme, localization, configuration }
