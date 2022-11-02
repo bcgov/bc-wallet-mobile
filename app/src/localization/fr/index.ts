2e91b6b0d8ea5ffa0cbda0ebaca71df852cfe322
Akiff Manji <akiff.manji@gmail.com>
2022-11-02 13:58:04 -0700

    Adds fr and pt-br lang placeholders for BC Wallet
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

---
+++ app/src/localization/fr/index.ts
@@ -0,0 +1,50 @@
+// prettier-ignore
+const translation = {
+  "BCID": {
+    "GetID": "Get BCID",
+    "GetDigitalID": "Get your BC Digital ID",
+  },
+  "Error": {
+    "Title2020": "Unable to parse invitation",
+    "Message2020": "There was a problem parsing the connection invitation.",
+    "Title2021": "Unable to receive invitation",
+    "Message2021": "There was a problem receiving the invitation to connect.",
+    "Title2022": "Unable to find legacy DID",
+    "Message2022": "There was a problem extracting the did repository.",
+    "Title2024": "BCSC Authentication",
+    "Message2024": "The authentication request was canceled.",
+    "Title2025": "BCSC Authentication",
+    "Message2025": "There was a problem reported by BCSC.",
+    "NoMessage": "No Message",
+  },
+  "Biometry": {
+    "Toggle": "Toggle Biometrics",
+    "EnabledText1": "Log in with your phone's biometrics instead of your wallet PIN.",
+    "EnabledText1Bold": "you will need to use biometrics to open your BC Wallet.",
+    "EnabledText2": "This means all fingerprint and facial data added on this phone can be used to access your BC Wallet.",
+    "EnabledText3": "Anyone who can access your phone with biometrics can access your BC Wallet.",
+    "EnabledText3Bold": "Ensure only you have access to your wallet.",
+    "Warning": "Ensure only you have access to your wallet.",
+    "UseToUnlock": "Use biometrics to unlock wallet?"
+  },
+  "Screens": {
+    "Onboarding": "BC Wallet",
+  },
+  "NetInfo": {
+    "NoInternetConnectionTitle": "No internet connection",
+    "NoInternetConnectionMessage": "You're unable to access services using BC Wallet or receive credentials until you're back online.\n\nPlease check your internet connection."
+  },
+  "Settings": {
+    "Help": "Help",
+    "MoreInformation": "More Information",
+    "HelpUsingBCWallet": "Help using BC Wallet",
+    "ReportAProblem": "Report A Problem",
+    "TermsOfUse": "Terms of use",
+    "PrivacyStatement": "Privacy statement",
+    "VulnerabilityDisclosurePolicy": "Vulnerability disclosure policy",
+    "Accessibility": "Accessibility",
+    "IntroductionToTheApp": "Introduction to the app"
+  }
+}
+
+export default translation
