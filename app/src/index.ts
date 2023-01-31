0992d494857d1a8b2cd2f86a8d6e80893f4ce8ac
Akiff Manji <amanji@petridish.dev>
2023-01-30 17:53:20 -0800

    Adds fallback mechanism for credential branding
    Signed-off-by: Akiff Manji <amanji@petridish.dev>

--- app/src/index.ts
+++ app/src/index.ts
@@ -6,24 +6,22 @@ import {
   indyLedgers,
   defaultConfiguration,
 } from 'aries-bifold'
-import { Bundles } from 'aries-bifold/lib/typescript/App/types/oca'
 import merge from 'lodash.merge'
+import { ReducerAction } from 'react'
 
 import bundles from './assets/branding/credential-branding'
 import AddCredentialButton from './components/AddCredentialButton'
 import AddCredentialSlider from './components/AddCredentialSlider'
-import BCIDView from './components/BCIDView'
 import EmptyList from './components/EmptyList'
+import { useNotifications } from './hooks/notifications'
 import en from './localization/en'
 import Developer from './screens/Developer'
 import { pages } from './screens/OnboardingPages'
+import PersonCredentialScreen from './screens/PersonCredential'
 import Splash from './screens/Splash'
 import Terms from './screens/Terms'
-import PersonCredentialScreen from './screens/PersonCredential'
-import { defaultTheme as theme } from './theme'
-import { useNotifications } from './hooks/notifications'
-import { ReducerAction } from 'react'
 import { BCDispatchAction } from './store'
+import { defaultTheme as theme } from './theme'
 
 const localization = merge({}, translationResources, {
   en: { translation: en },
@@ -39,7 +37,9 @@ const configuration: ConfigurationContext = {
   credentialListOptions: AddCredentialSlider,
   credentialEmptyList: EmptyList,
   developer: Developer,
-  OCABundle: new types.oca.DefaultOCABundleResolver().loadBundles(bundles as unknown as Bundles),
+  OCABundleResolver: new types.oca.OCABundleResolver(bundles as unknown as types.oca.Bundles, {
+    cardOverlayType: types.oca.CardOverlayType.CardLayout11,
+  }),
   record: Record,
   indyLedgers: selectedLedgers,
   settings: [],
