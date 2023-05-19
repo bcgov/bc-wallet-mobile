091200b80dc40d6705668b84ed74be1670b52cc2
Bryce McMath <32586431+bryce-mcmath@users.noreply.github.com>
2023-05-19 09:41:22 -0700

    feat: add survey links to settings menu (#1113)
    Signed-off-by: Bryce McMath <bryce.j.mcmath@gmail.com>

--- app/index.js
+++ app/index.js
@@ -27,6 +27,7 @@ import { AppRegistry } from 'react-native'
 import App from './App'
 import { name as appName } from './app.json'
 import bcwallet from './src/'
+
 const { theme } = bcwallet
 
 const { ColorPallet } = theme
