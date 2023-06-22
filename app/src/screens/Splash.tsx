b34e76162405b9208dae7cfb0cb1eda8a11828a7
Jason C. Leach <jason.leach@fullboar.ca>
2023-06-22 13:27:00 -0700

    fix: ios working
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- app/src/screens/Splash.tsx
+++ app/src/screens/Splash.tsx
@@ -18,6 +18,7 @@ import {
   OnboardingState,
   LoginAttemptState,
   PreferencesState,
+  MigrationState,
   ToursState,
   useAuth,
   useTheme,
@@ -26,6 +27,8 @@ import {
   InfoBox,
   InfoBoxType,
   testIdWithKey,
+  didMigrateToAskar,
+  migrateToAskar,
 } from 'aries-bifold'
 import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
@@ -208,6 +211,16 @@ const Splash: React.FC = () => {
           })
         }
 
+        const migrationData = await AsyncStorage.getItem(LocalStorageKeys.Migration)
+        if (migrationData) {
+          const dataAsJSON = JSON.parse(migrationData) as MigrationState
+
+          dispatch({
+            type: DispatchAction.MIGRATION_UPDATED,
+            payload: [dataAsJSON],
+          })
+        }
+
         const toursData = await AsyncStorage.getItem(LocalStorageKeys.Tours)
         if (toursData) {
           const dataAsJSON = JSON.parse(toursData) as ToursState
@@ -307,28 +320,25 @@ const Splash: React.FC = () => {
           }),
         }
 
-        // const options = {
-        //   config: {
-        //     label: 'BC Wallet',
-        //     mediatorConnectionsInvite: Config.MEDIATOR_URL,
-        //     mediatorPickupStrategy: MediatorPickupStrategy.Implicit,
-        //     walletConfig: { id: credentials.id, key: credentials.key },
-        //     autoAcceptConnections: true,
-        //     autoAcceptCredentials: AutoAcceptCredential.ContentApproved,
-        //     logger: new ConsoleLogger(LogLevel.trace),
-        //     indyLedgers,
-        //     connectToIndyLedgersOnStartup: false,
-        //     autoUpdateStorageOnStartup: true,
-        //   },
-        //   dependencies: agentDependencies,
-        // }
-
         const newAgent = new Agent(options)
         const wsTransport = new WsOutboundTransport()
         const httpTransport = new HttpOutboundTransport()
 
         newAgent.registerOutboundTransport(wsTransport)
         newAgent.registerOutboundTransport(httpTransport)
+
+        // If we haven't migrated to Aries Askar yet, we need to do this before we initialize the agent.
+        if (!didMigrateToAskar(store.migration)) {
+          newAgent.config.logger.debug('Agent not updated to Aries Askar, updating...')
+
+          await migrateToAskar(credentials.id, credentials.key, newAgent)
+
+          newAgent.config.logger.debug('Successfully finished updating agent to Aries Askar')
+          // Store that we migrated to askar.
+          dispatch({
+            type: DispatchAction.DID_MIGRATE_TO_ASKAR,
+          })
+        }
 
         setStep(6)
         await newAgent.initialize()
