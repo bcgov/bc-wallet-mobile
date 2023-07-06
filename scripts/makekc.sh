4b47d5f3eab699673a3e1c0d3a632fad9313cb0c
Jason C. Leach <jason.leach@fullboar.ca>
2023-07-06 15:12:05 -0700

    chore: work on makekc script
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- scripts/makekc.sh
+++ scripts/makekc.sh
@@ -3,9 +3,6 @@ set -xaou pipefail
 
 CERT_PATH=$RUNNER_TEMP/certificates.p12
 KC_NAME=cicd.keychain
-echo "1find /Users/runner/Library/Keychains -iname '$KC_NAME-db'"
-echo "2find /Users/runner/Library/Keychains -iname '${KC_NAME}-db'"
-
 KC_FILE_COUNT=$(find /Users/runner/Library/Keychains -iname $KC_NAME-db | wc -l | tr -d " ")
 
 echo ">> Build Keychain Starting... 🤞"
@@ -22,19 +19,21 @@ if [[ $KC_FILE_COUNT -eq 0 ]]; then
   /usr/bin/security unlock-keychain -p $1 $KC_NAME
   /usr/bin/security list-keychains -d user -s $KC_NAME
 
-  find /Users/runner/Library/Keychains
+  echo ">> Importing Certificate"
+  /usr/bin/security import $CERT_PATH -P "$1" -t cert -f pkcs12 -k $KC_NAME -T /usr/bin/codesign -T /usr/bin/security
+  /usr/bin/security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $1 $KC_NAME
+  /usr/bin/security set-keychain-settings -lut 21600 $KC_NAME
+
 else
   echo ">> Keychain $KC_NAME exists. Skipping create..."
+  echo ">> Importing Certificate"
+  /usr/bin/security import $CERT_PATH -P "$1" -t cert -f pkcs12 -k $KC_NAME -T /usr/bin/codesign -T /usr/bin/security
 fi  
 # # create temporary keychain
 # security create-keychain -p "$1" $KEYCHAIN_PATH
 # security set-keychain-settings -lut 21600 $KEYCHAIN_PATH
 # security unlock-keychain -p "$1" $KEYCHAIN_PATH
 
-echo ">> Importing Certificate"
-/usr/bin/security import $CERT_PATH -P "$1" -t cert -f pkcs12 -k $KC_NAME -T /usr/bin/codesign -T /usr/bin/security
-/usr/bin/security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $1 $KC_NAME
-/usr/bin/security set-keychain-settings -lut 21600 $KC_NAME
 
 # # import certificate to keychain
 # security import $CERT_PATH -P "$1" -A -t cert -f pkcs12 -k $KEYCHAIN_PATH
