a03eb4445170f978f73272af8e99d25a9cb0909d
Jason C. Leach <jason.leach@fullboar.ca>
2023-07-10 12:19:12 -0700

    fix: work on keychain script
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- scripts/makekc.sh
+++ scripts/makekc.sh
@@ -18,22 +18,21 @@ if [[ $KC_FILE_COUNT -eq 0 ]]; then
   /usr/bin/security default-keychain -s $KC_NAME
   /usr/bin/security unlock-keychain -p $1 $KC_NAME
   /usr/bin/security list-keychains -d user -s $KC_NAME
-
-  echo ">> Importing Certificate"
-  /usr/bin/security import $CERT_PATH -P "$1" -t cert -f pkcs12 -k $KC_NAME -T /usr/bin/codesign -T /usr/bin/security
-  /usr/bin/security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $1 $KC_NAME
-  /usr/bin/security set-keychain-settings -lut 21600 $KC_NAME
-
 else
   echo ">> Keychain $KC_NAME exists. Skipping create..."
-  echo ">> Importing Certificate"
-  /usr/bin/security import $CERT_PATH -P "$1" -t cert -f pkcs12 -k $KC_NAME -T /usr/bin/codesign -T /usr/bin/security
-fi  
+fi
+
+echo ">> Importing Certificate"
+/usr/bin/security import $CERT_PATH -P "$1" -t cert -f pkcs12 -k $KC_NAME -T /usr/bin/codesign -T /usr/bin/security
+
+echo ">> Update Keychain Settings"
+/usr/bin/security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $1 $KC_NAME
+/usr/bin/security set-keychain-settings -lut 21600 $KC_NAME
+
 # # create temporary keychain
 # security create-keychain -p "$1" $KEYCHAIN_PATH
 # security set-keychain-settings -lut 21600 $KEYCHAIN_PATH
 # security unlock-keychain -p "$1" $KEYCHAIN_PATH
-
 
 # # import certificate to keychain
 # security import $CERT_PATH -P "$1" -A -t cert -f pkcs12 -k $KEYCHAIN_PATH
