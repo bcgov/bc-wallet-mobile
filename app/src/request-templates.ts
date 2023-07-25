b2fef24cb0327c3bf061e4342a33562f03591312
Wade King <wkingnumber2@gmail.com>
2023-07-25 15:07:18 -0700

    feat: corrections proof template (#1294)
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- app/src/request-templates.ts
+++ app/src/request-templates.ts
@@ -33,10 +33,32 @@ const memberCardRestrictions = [
   { schema_id: 'AuJrigKQGRLJajKAebTgWu:2:Member Card:1.5.1', issuer_did: 'AuJrigKQGRLJajKAebTgWu' }, // Test
   // BC Wallet Showcase
   { schema_id: 'XUxBrVSALWHLeycAUhrNr9:2:Member Card:1.5.1', issuer_did: 'XUxBrVSALWHLeycAUhrNr9' }, // Prod
-  { schema_id: '2K2h7kf8VGTLtfoxJgWazf:2:Member Card:1.1', issuer_did: '2K2h7kf8VGTLtfoxJgWazf' }, // Dev & Test
+  { schema_id: 'GmVzJEQG8kmprdbFRogWQr:2:member_card:1.51', issuer_did: 'GmVzJEQG8kmprdbFRogWQr' }, // Dev & Test
 ]
+const memberCardCodeRestrictions = memberCardRestrictions.map((restriction) => {
+  return { ...restriction, 'attr::Member Status::value': 'Practising', 'attr::Member Status Code::value': 'PRAC' }
+})
 
 export const proofRequestTemplates: Array<ProofRequestTemplate> = [
+  {
+    id: 'BC:5:PracticingLawyerAndPhoto:0.0.1:indy',
+    name: 'Practicing Lawyer and Person Photo Attribute',
+    description: 'Verify that someone is a practising lawyer with photo ID',
+    version: '0.0.1',
+    payload: {
+      type: ProofRequestType.AnonCreds,
+      data: [
+        {
+          schema: personSchema,
+          requestedAttributes: [{ names: ['given_names', 'family_name', 'picture'], restrictions: personRestrictions }],
+        },
+        {
+          schema: memberCardSchema,
+          requestedAttributes: [{ names: ['Given Name', 'Surname'], restrictions: memberCardCodeRestrictions }],
+        },
+      ],
+    },
+  },
   {
     id: 'BC:5:FullName:0.0.1:indy',
     name: 'Full name',
