645d5260b77a092da75215559d1daaec65ecb8a1
Clécio Varjão <1348549+cvarjao@users.noreply.github.com>
2023-09-29 00:38:15 -0700

    fix ios build
    Signed-off-by: Clécio Varjão <1348549+cvarjao@users.noreply.github.com>

--- app/ios/AriesBifold/AppDelegate.m
+++ app/ios/AriesBifold/AppDelegate.m
@@ -3,6 +3,8 @@
 #import <Firebase.h>
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
+#import <React/RCTLinkingManager.h>
 
 @implementation AppDelegate
 
