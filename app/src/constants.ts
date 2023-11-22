b5abae0ddbb53d8dacd03b1896ed48d46ec059dd
Jason C. Leach <jason.leach@fullboar.ca>
2023-11-22 11:37:11 -0800

    feat: implement new scans screen  (#1614)
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- app/src/constants.ts
+++ app/src/constants.ts
@@ -1,3 +1,4 @@
 export const surveyMonkeyUrl = 'https://www.surveymonkey.com/r/7BMHJL8'
 export const surveyMonkeyExitUrl = 'https://www.surveymonkey.com/survey-thanks'
+export const whereToUseWalletUrl = 'https://www2.gov.bc.ca/gov/content/governments/government-id/bc-wallet#where'
 export const hitSlop = { top: 44, bottom: 44, left: 44, right: 44 }
