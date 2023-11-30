4b1287e5f46086aa9556f6bbc007de47ceb3c475
Jason C. Leach <jason.leach@fullboar.ca>
2023-11-30 10:02:56 -0800

    fix: override url (#1637)
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- app/src/constants.ts
+++ app/src/constants.ts
@@ -1,4 +1,3 @@
 export const surveyMonkeyUrl = 'https://www.surveymonkey.com/r/7BMHJL8'
 export const surveyMonkeyExitUrl = 'https://www.surveymonkey.com/survey-thanks'
-export const whereToUseWalletUrl = 'https://www2.gov.bc.ca/gov/content/governments/government-id/bc-wallet#where'
 export const hitSlop = { top: 44, bottom: 44, left: 44, right: 44 }
