2ec19032eb51050247ef865aabbbeea8ec5f0853
Bryce McMath <bryce.j.mcmath@gmail.com>
2024-02-05 17:36:58 -0800

    feat: credential help actions
    Signed-off-by: Bryce McMath <bryce.j.mcmath@gmail.com>

--- app/src/index.ts
+++ app/src/index.ts
@@ -4,10 +4,13 @@ import {
   Record,
   indyLedgers,
   defaultConfiguration,
+  Stacks,
+  Screens,
 } from '@hyperledger/aries-bifold-core'
 import { BrandingOverlayType, RemoteOCABundleResolver } from '@hyperledger/aries-oca/build/legacy'
 import merge from 'lodash.merge'
 import { ReducerAction } from 'react'
+import { Linking } from 'react-native'
 import { Config } from 'react-native-config'
 
 import AddCredentialButton from './components/AddCredentialButton'
@@ -75,6 +78,35 @@ const configuration: ConfigurationContext = {
   useCustomNotifications: useNotifications,
   useAttestation: useAttestation,
   whereToUseWalletUrl: 'https://www2.gov.bc.ca/gov/content/governments/government-id/bc-wallet#where',
+  getCredentialHelpDictionary: [
+    {
+      // Person Credential
+      credDefIds: [
+        'KCxVC8GkKywjhWJnUfCmkW:3:CL:20:PersonQA',
+        '7xjfawcnyTUcduWVysLww5:3:CL:28075:PersonSIT',
+        'XpgeQa93eZvGSZBZef3PHn:3:CL:28075:PersonDEV',
+        'RGjWbW1eycP7FrMf4QJvX8:3:CL:13:Person',
+      ],
+      action: (navigation) => {
+        navigation.getParent()?.navigate(Stacks.NotificationStack, {
+          screen: Screens.CustomNotification,
+        })
+      },
+    },
+    {
+      // Member Card
+      credDefIds: [
+        '4xE68b6S5VRFrKMMG1U95M:3:CL:59232:default',
+        'L6ASjmDDbDH7yPL1t2yFj9:2:member_card:1.53',
+        'M6dhuFj5UwbhWkSLmvYSPc:2:member_card:1.53',
+        'QEquAHkM35w4XVT3Ku5yat:2:member_card:1.53',
+        'AuJrigKQGRLJajKAebTgWu:3:CL:209526:default',
+      ],
+      action: () => {
+        Linking.openURL('https://www.lawsociety.bc.ca/lsbc/apps/members/login.cfm')
+      },
+    },
+  ],
 }
 
 export default { theme, localization, configuration }
