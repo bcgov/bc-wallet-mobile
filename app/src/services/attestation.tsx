f2c806423dc47e3aff11f1830e1c840c720b06e4
Jason C. Leach <jason.leach@fullboar.ca>
2024-02-08 11:07:41 -0800

    fix: add test env cred_def_id
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- app/src/services/attestation.tsx
+++ app/src/services/attestation.tsx
@@ -73,6 +73,7 @@ const attestationCredDefIds = [
   'J6LCm5Edi9Mi3ASZCqNC1A:3:CL:109799:dev-attestation',
   'NxWbeuw8Y2ZBiTrGpcK7Tn:3:CL:48312:default',
   'NXp6XcGeCR2MviWuY51Dva:3:CL:33557:bcwallet',
+  'RycQpZ9b4NaXuT5ZGjXkUE:3:CL:120:bcwallet',
 ]
 
 // proof requests can vary wildly but we'll know attestation requests must contain the cred def id as a restriction
