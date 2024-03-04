d6b9c0a941bccda1e85a61403d16d3617069e11b
Bryce McMath <32586431+bryce-mcmath@users.noreply.github.com>
2024-03-04 15:54:39 -0800

    chore: re-enable multi use and update packages (#1839)
    Signed-off-by: Bryce McMath <bryce.j.mcmath@gmail.com>

--- app/src/index.ts
+++ app/src/index.ts
@@ -77,6 +77,7 @@ const configuration: ConfigurationContext = {
   disableOnboardingSkip: true,
   useCustomNotifications: useNotifications,
   useAttestation: useAttestation,
+  enableUseMultUseInvitation: true,
   whereToUseWalletUrl: 'https://www2.gov.bc.ca/gov/content/governments/government-id/bc-wallet#where',
   getCredentialHelpDictionary: [
     {
