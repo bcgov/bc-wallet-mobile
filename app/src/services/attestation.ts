5ee44caaa393853802c83bb113b00cc30a0e1659
Jason C. Leach <jason.leach@fullboar.ca>
2024-07-24 11:23:47 -0700

    feat: work on attestation everywhere
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- app/src/services/attestation.ts
+++ app/src/services/attestation.ts
@@ -31,6 +31,7 @@ const defaultResponseTimeoutInMs = 10000 // DRPC response timeout
 export type AttestationMonitorOptions = {
   attestationInviteUrl: string
   attestationCredDefIds: string[]
+  shouldHandleProofRequestAutomatically?: boolean
 }
 
 export const AttestationEventTypes = {
@@ -40,6 +41,9 @@ export const AttestationEventTypes = {
   FailedHandleProof: 'AttestationEvent.FailedHandleProof',
   FailedRequestCredential: 'AttestationEvent.FailedRequestCredential',
 } as const
+
+type AttestationEventTypeKeys = keyof typeof AttestationEventTypes
+type AttestationEventTypeValues = (typeof AttestationEventTypes)[AttestationEventTypeKeys]
 
 export interface AttestationCredentialFormat {
   attributes: {
@@ -104,6 +108,9 @@ export class AttestationMonitor {
   private options: AttestationMonitorOptions
   private log?: BaseLogger
   private _attestationWorkflowInProgress = false
+  private _shouldHandleProofRequestAutomatically = false
+  private _proofRequest?: ProofExchangeRecord
+  // private _currentWorkflowState?: typeof AttestationEventTypes
 
   // take in options, agent, and logger. Options should include the attestation service URL
   // and the proof to watch for along with the cred_ef_id of the attestation credentials.
@@ -111,10 +118,22 @@ export class AttestationMonitor {
     this.agent = agent
     this.log = logger
     this.options = options
+    const { shouldHandleProofRequestAutomatically } = options
+    this._shouldHandleProofRequestAutomatically = shouldHandleProofRequestAutomatically ?? false
+
+    console.log('aytio proof === ', this._shouldHandleProofRequestAutomatically)
   }
 
   public get attestationWorkflowInProgress() {
     return this._attestationWorkflowInProgress
+  }
+
+  public set shouldHandleProofRequestAutomatically(value: boolean) {
+    this._shouldHandleProofRequestAutomatically = value
+  }
+
+  public get shouldHandleProofRequestAutomatically() {
+    return this._shouldHandleProofRequestAutomatically
   }
 
   public async start(): Promise<void> {
@@ -135,8 +154,7 @@ export class AttestationMonitor {
   public requestAttestationCredential = async () => {
     this.log?.info('Fetching attestation credential')
 
-    this._attestationWorkflowInProgress = true
-    DeviceEventEmitter.emit(AttestationEventTypes.Started)
+    this.startWorkflow()
 
     try {
       const connection = await this.connectToAttestationAgent()
@@ -179,11 +197,37 @@ export class AttestationMonitor {
         )
       }
     } catch (error) {
-      this._attestationWorkflowInProgress = false
       this.log?.error('Failed to fetch attestation credential', error as Error)
 
-      DeviceEventEmitter.emit(AttestationEventTypes.FailedRequestCredential, error)
+      this.stopWorkflow(AttestationEventTypes.FailedRequestCredential, error as Error)
     }
+  }
+
+  private startWorkflow = () => {
+    this._attestationWorkflowInProgress = true
+    DeviceEventEmitter.emit(AttestationEventTypes.Started)
+  }
+
+  private stopWorkflow = (eventType: AttestationEventTypeValues, error?: Error) => {
+    this._attestationWorkflowInProgress = false
+    this._proofRequest = undefined
+    DeviceEventEmitter.emit(eventType, error)
+  }
+
+  private handleProofRequest = async (proofRequest: ProofExchangeRecord): Promise<boolean> => {
+    this.log?.info('Selecting credentials for attestation proof request')
+    // This will throw if we don't have the necessary credentials
+    const credentials = await this.agent.proofs.selectCredentialsForRequest({
+      proofRecordId: proofRequest.id,
+    })
+
+    this.log?.info('Accepting attestation proof request')
+    await this.agent.proofs.acceptRequest({
+      proofRecordId: proofRequest.id,
+      proofFormats: credentials.proofFormats,
+    })
+
+    return true
   }
 
   private handleCredentialStateChanged = async (event: BaseEvent) => {
@@ -215,14 +259,21 @@ export class AttestationMonitor {
         // TODO: credential.offer in flight completed
         this.log?.info('Credential accepted')
 
-        this._attestationWorkflowInProgress = false
-
-        DeviceEventEmitter.emit(AttestationEventTypes.Completed)
+        if (this._shouldHandleProofRequestAutomatically && this._proofRequest) {
+          if (this._proofRequest.state === ProofState.RequestReceived) {
+            const result = await this.handleProofRequest(this._proofRequest)
+            if (result) {
+              this.stopWorkflow(AttestationEventTypes.Completed)
+            }
+          } else {
+            this.stopWorkflow(AttestationEventTypes.Completed)
+          }
+        }
       }
     } catch (error) {
       this.log?.error('Failed to handle credential offer', error as Error)
 
-      DeviceEventEmitter.emit(AttestationEventTypes.FailedHandleOffer, error)
+      this.stopWorkflow(AttestationEventTypes.FailedHandleOffer, error as Error)
     }
   }
 
@@ -241,10 +292,18 @@ export class AttestationMonitor {
     try {
       // 1. Is the proof requesting an attestation credential
       if (!(await isProofRequestingAttestation(proof, this.agent, this.options.attestationCredDefIds))) {
+        this.log?.info('Proof nope!!!!')
+
         return
       }
 
       this.log?.info('Proof is requesting attestation')
+
+      if (this._shouldHandleProofRequestAutomatically) {
+        this._proofRequest = proof
+
+        this.startWorkflow()
+      }
 
       // 2. Does the wallet have a valid attestation credential that will
       // satisfy the proof request?
@@ -252,7 +311,16 @@ export class AttestationMonitor {
 
       // 3. If yes, do nothing
       if (!required) {
-        this.log?.info('Valid credentials already exist, nothing to do')
+        this.log?.info('Valid credentials already exist, checking automatic handling')
+
+        if (this._shouldHandleProofRequestAutomatically && this._proofRequest) {
+          this.log?.info('Handling proof request automatically')
+
+          const result = await this.handleProofRequest(proof)
+          if (result) {
+            this.stopWorkflow(AttestationEventTypes.Completed)
+          }
+        }
 
         return
       }
@@ -262,7 +330,7 @@ export class AttestationMonitor {
     } catch (error) {
       this.log?.error('Failed to handle proof', error as Error)
 
-      DeviceEventEmitter.emit(AttestationEventTypes.FailedHandleProof, error)
+      this.stopWorkflow(AttestationEventTypes.FailedHandleProof, error as Error)
     }
   }
 
