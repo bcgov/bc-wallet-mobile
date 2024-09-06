1011502386e376f802af4b5d08d48bb4a86209c7
Filipe Santos <48060475+fc-santos@users.noreply.github.com>
2024-09-06 14:24:21 -0400

    fix: android cashes on opening (#113)
    Signed-off-by: fc-santos <filipesantos@live.ca>

--- app/android/app/src/main/java/ca/qc/gouv/QCWallet/MainActivity.java
+++ app/android/app/src/main/java/ca/qc/gouv/QCWallet/MainActivity.java
@@ -1,21 +1,18 @@
 package ca.qc.gouv.portefeuillemobileqc;
 import android.os.Bundle;
-import java.io.File;
-
 import com.facebook.react.ReactActivity;
+import java.io.File;
 
 public class MainActivity extends ReactActivity {
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
+   * Returns the name of the main component registered from JavaScript. This is
+   * used to schedule rendering of the component.
    */
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
   }
-  
-  protected String getMainComponentName() {
-    return "QCWallet";
-  }
+
+  protected String getMainComponentName() { return "PortefeuilleQc"; }
 }
