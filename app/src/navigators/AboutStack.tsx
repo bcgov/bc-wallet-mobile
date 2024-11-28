20b57fafc7e0f455088c4bd4d0ddad1c5fb0c3d0
ClaudeArs <107423901+ClaudeArs@users.noreply.github.com>
2024-11-28 09:09:14 -0500

    Translate header About page (#166)
    Signed-off-by: ClaudeArs <claudears2474@gmail.com>

--- app/src/navigators/AboutStack.tsx
+++ app/src/navigators/AboutStack.tsx
@@ -2,6 +2,7 @@ import { useTheme } from '@hyperledger/aries-bifold-core'
 import { useDefaultStackOptions } from '@hyperledger/aries-bifold-core/App/navigators/defaultStackOptions'
 import { createStackNavigator } from '@react-navigation/stack'
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 
 import About from '../screens/About'
 
@@ -11,13 +12,20 @@ const AboutStack: React.FC = () => {
   const StackAbout = createStackNavigator<AboutStackParams>()
   const theme = useTheme()
   const defaultStackOptions = useDefaultStackOptions(theme)
+  const { t } = useTranslation()
 
   return (
     <StackAbout.Navigator
       initialRouteName={Screens.About}
       screenOptions={{ ...defaultStackOptions, headerShown: true }}
     >
-      <StackAbout.Screen name={Screens.About} component={About} />
+      <StackAbout.Screen
+        name={Screens.About}
+        component={About}
+        options={{
+          title: t('Screens.About'),
+        }}
+      />
     </StackAbout.Navigator>
   )
 }
