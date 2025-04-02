408f42e1d2ee4b4a918e2f53475d64e2dc146e9a
Bryce McMath <bryce.j.mcmath@gmail.com>
2025-04-02 14:58:46 -0700

    chore(deps): update bifold pkgs, housekeeping
    Signed-off-by: Bryce McMath <bryce.j.mcmath@gmail.com>

--- app/index.js
+++ app/index.js
@@ -1,10 +1,5 @@
-/* eslint-disable import/no-extraneous-dependencies */
-/**
- * @format
- */
 import 'fast-text-encoding' // polyfill for TextEncoder and TextDecoder
 import 'react-native-gesture-handler'
-// remove these when updated to react-native 0.65.0
 import '@formatjs/intl-getcanonicallocales/polyfill'
 import '@formatjs/intl-locale/polyfill'
 import '@formatjs/intl-pluralrules/polyfill'
