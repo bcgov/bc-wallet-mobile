fbe6c3f3e8cfddb4beeb865607ebcb72a7aeeaca
Jason C. Leach <jason.leach@fullboar.ca>
2025-04-25 14:07:15 -0700

    feat: expose ios key access
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- packages/bcsc-core/src/index.ts
+++ packages/bcsc-core/src/index.ts
@@ -1,6 +1,18 @@
 import { NativeModules, Platform } from 'react-native';
 import NativeBcscCoreSpec from './NativeBcscCore';
 
+export interface PrivateKeyInfo {
+  keyType: string;
+  keySize: number;
+  tag: string;
+  created: number; // Timestamp
+}
+
+export interface KeyPair {
+  public: string;
+  private: string;
+}
+
 const LINKING_ERROR =
   `The package 'react-native-attestation' doesn't seem to be linked. Make sure: \n\n` +
   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
@@ -25,10 +37,14 @@ const BcscCore = BcscCoreModule
       }
     );
 
-export function multiply(a: number, b: number): number {
+export const multiply = (a: number, b: number): number => {
   return BcscCore.multiply(a, b);
-}
+};
 
-export function multiply2(a: number, b: number): number {
-  return BcscCore.multiply2(a, b);
-}
+export const findAllPrivateKeys = (): Promise<PrivateKeyInfo[]> => {
+  return BcscCore.findAllPrivateKeys();
+};
+
+export const getKeyPair = (label: string): Promise<KeyPair> => {
+  return BcscCore.getKeyPair(label);
+};
