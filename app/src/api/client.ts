40318c950733a0688e731c679ec8a18ceba4f6ac
al-rosenthal <alfred.k.rosenthal@gmail.com>
2025-05-02 16:45:29 -0700

    feat: added jose, updated api client
    Signed-off-by: al-rosenthal <alfred.k.rosenthal@gmail.com>

--- app/src/api/client.ts
+++ app/src/api/client.ts
@@ -1,34 +1,70 @@
-import axios from 'axios'
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse, InternalAxiosRequestConfig } from 'axios'
 import Config from 'react-native-config'
 
-const apiClient = axios.create({
-  baseURL: Config.IAS_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-})
+class ApiClient {
+  private client: AxiosInstance
+  private authToken: string | null = null
 
-// Add a request interceptor
-apiClient.interceptors.request.use(
-  (config) => {
-    // add auth headers to request
-    // const token = 'your-auth-token';
-    // if (token) {
-    //   config.headers.Authorization = `Bearer ${token}`;
-    // }
+  constructor(baseURL: string = String(Config.IAS_URL)) {
+    this.client = axios.create({
+      baseURL,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+
+    // Add interceptors
+    this.client.interceptors.request.use(this.handleRequest.bind(this))
+  }
+
+  // Set the authentication token
+  setAuthToken(token: string) {
+    this.authToken = token
+  }
+
+  buildToken() {
+    // getKeys (jason module)
+    // fetch latest ID from keys
+    // jose.generateKeyPair
+    // sign the token
+    // return the token
+  }
+
+  // Clear the authentication token
+  clearAuthToken() {
+    this.authToken = null
+  }
+
+  // Handle request interception
+  private handleRequest(config: InternalAxiosRequestConfig): InternalAxiosRequestConfig {
+    if (this.authToken) {
+      config.headers.set('Authorization', `Bearer ${this.authToken}`)
+    }
     return config
-  },
-  (error: Error) => {
+  }
+
+  // Handle errors globally
+  private handleError(error: Error): Promise<any> {
+    // Log or handle errors here
     return Promise.reject(error)
   }
-)
 
-export default apiClient
+  // Example methods for API calls
+  async get<T>(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+    return this.client.get<T>(url, config)
+  }
 
-/*
-  1. use module to fetch device keys
-  2. find device key that is the correct ID
-  3. Use Jose to generate keys with that ID
-  4. Use Jose to generate a JWT with the keys
-  5. Send authenticated requests to the protected server routes
-*/
+  async post<T>(url: string, data?: any, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+    return this.client.post<T>(url, data, config)
+  }
+
+  async put<T>(url: string, data?: any, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+    return this.client.put<T>(url, data, config)
+  }
+
+  async delete<T>(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+    return this.client.delete<T>(url, config)
+  }
+}
+
+export default new ApiClient()
