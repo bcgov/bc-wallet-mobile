1c052f2358c5c507ad11b15fd8ab3b15a52c35c9
jamshale <jamiehalebc@gmail.com>
2025-06-27 12:20:23 -0700

    feat(pickup): Manually batch pickup queued messages
    Signed-off-by: jamshale <jamiehalebc@gmail.com>

---
+++ app/src/utils/mediator.ts
@@ -0,0 +1,13 @@
+import { Agent, MediatorPickupStrategy } from '@credo-ts/core'
+
+export const batchPickup = async (agent: Agent): Promise<void> => {
+  try {
+    for (let i = 0; i < 10; i++) {
+      agent.config.logger.debug(`Batch pickup attempt ${i + 1}`)
+      agent.mediationRecipient.initiateMessagePickup(undefined, MediatorPickupStrategy.Implicit)
+      await new Promise((resolve) => setTimeout(resolve, 50)) // wait for .05 seconds before next pickup
+    }
+  } catch (error) {
+    agent.config.logger.error(`Error during batch pickup: ${error}`)
+  }
+}
