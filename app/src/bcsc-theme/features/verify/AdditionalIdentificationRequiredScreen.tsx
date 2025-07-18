eef0427f9edfe6786f1f8cdef7957feb501d2c5a
al-rosenthal <alfred.k.rosenthal@gmail.com>
2025-07-18 10:43:42 -0700

    feat: added more ui to additional info screen
    Signed-off-by: al-rosenthal <alfred.k.rosenthal@gmail.com>

--- app/src/bcsc-theme/features/verify/AdditionalIdentificationRequiredScreen.tsx
+++ app/src/bcsc-theme/features/verify/AdditionalIdentificationRequiredScreen.tsx
@@ -1,9 +1,10 @@
 import { BCSCScreens, BCSCVerifyIdentityStackParams } from '@/bcsc-theme/types/navigators'
 import { Button, ButtonType, ThemedText, useTheme } from '@bifold/core'
 import { StackNavigationProp } from '@react-navigation/stack'
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { ScrollView } from 'react-native-gesture-handler'
+import { Icon } from 'react-native-vector-icons/Icon'
 
 type AdditionalIdentificationRequiredScreenProps = {
   navigation: StackNavigationProp<BCSCVerifyIdentityStackParams, BCSCScreens.AdditionalIdentificationRequired>
@@ -32,11 +33,29 @@ const AdditionalIdentificationRequiredScreen: React.FC<AdditionalIdentificationR
   return (
     <SafeAreaView style={styles.pageContainer} edges={['bottom', 'left', 'right']}>
       <ScrollView contentContainerStyle={styles.scrollView}>
+        <Image source={{ uri: '' }} />
         <View>
-          <ThemedText>
+          <ThemedText variant={'headingThree'}>
             {'You must provide additional ID case your BC Services Card does not have a photo on it.'}
           </ThemedText>
+          <ThemedText>
+            {`It's needed to verify your identity. You'll be asked to provide one or two government-issued IDs`}
+          </ThemedText>
         </View>
+        <View>
+          <ThemedText variant={'headingThree'}>{'Check your ID'}</ThemedText>
+          <ThemedText>{'Has the same name as on your BC Services Card'}</ThemedText>
+          <ThemedText>{'Has a recent photo'}</ThemedText>
+          <ThemedText>{'Is not expired'}</ThemedText>
+        </View>
+        <View>
+          <ThemedText variant={'headingThree'}>{'Limited access to services'}</ThemedText>
+          <ThemedText>{`Some services only accept the app when it's set up with a BC Services Card with a photo.`}</ThemedText>
+        </View>
+        <TouchableOpacity onPress={() => console.log('OPEN SO COOL WEBVIEW')}>
+          <Text style={{ color: ColorPallet.brand.primary, marginTop: Spacing.sm }}>{'Which services?'}</Text>
+          <Icon name={'help-circle'} />
+        </TouchableOpacity>
         <View style={{ marginTop: Spacing.md }}>
           <Button
             title={'Choose ID'}
