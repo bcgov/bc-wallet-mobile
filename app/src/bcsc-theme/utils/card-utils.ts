10e5b21ff6ddbf6915985fe9004c4246424382ca
Mac Deluca <Mac.Deluca@quartech.com>
2025-09-11 12:06:38 -0700

    feat: added getCardProcessForCardType utility function
    Signed-off-by: Mac Deluca <Mac.Deluca@quartech.com>

---
+++ app/src/bcsc-theme/utils/card-utils.ts
@@ -0,0 +1,26 @@
+import { BCSCCardProcess } from '../api/hooks/useAuthorizationApi'
+import { BCSCCardType } from '../types/cards'
+
+/**
+ * Get the card process for a given card type.
+ *
+ * @throws {Error} If the card type is invalid or None.
+ * @param {BCSCCardType} cardType - The type of BCSC card.
+ * @returns {*} {BCSCCardProcess} The corresponding card process.
+ */
+export function getCardProcessForCardType(cardType: BCSCCardType): BCSCCardProcess {
+  switch (cardType) {
+    case BCSCCardType.Combined:
+      return BCSCCardProcess.BCSC
+    case BCSCCardType.Photo:
+      return BCSCCardProcess.BCSC
+    case BCSCCardType.NonPhoto:
+      return BCSCCardProcess.BCSCNonPhoto
+    case BCSCCardType.Other:
+      return BCSCCardProcess.NonBCSC
+    case BCSCCardType.None:
+      throw new Error(`Invalid card type: ${BCSCCardType.None}}`)
+    default:
+      throw new Error(`Unknown card type: ${cardType}`)
+  }
+}
