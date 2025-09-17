bfcf2504d6e11209f1063be5c88dac3e30a4f059
Mac Deluca <Mac.Deluca@quartech.com>
2025-09-17 16:30:28 -0700

    chore: updated return type of usePairingApi
    Signed-off-by: Mac Deluca <Mac.Deluca@quartech.com>

--- app/src/bcsc-theme/api/hooks/usePairingApi.tsx
+++ app/src/bcsc-theme/api/hooks/usePairingApi.tsx
@@ -4,26 +4,38 @@ import { signPairingCode } from 'react-native-bcsc-core'
 import BCSCApiClient from '../client'
 import { withAccount } from './withAccountGuard'
 
-// There is no actual data response (just a 200) from the pairing code login endpoint, so we
-// define a minimal response for DX and and TypeScript
-export interface PairingCodeLoginResponseData {
-  success: boolean
+export interface PairingCodeLoginClientMetadata {
+  transaction_id: string
+  client_ref_id: string
+  client_name: string
+  client_uri: string
+  initiate_login_uri: string
+  policy_uri: string
+  aaplication_type: string
+  useage_date: string
 }
 
 const usePairingApi = (apiClient: BCSCApiClient) => {
+  /**
+   * Logs in a user using a pairing code and returns the client metadata.
+   *
+   * @param {string} code - The pairing code to use for login.
+   * @returns {*} {Promise<PairingCodeLoginClientMetadata>} A promise that resolves to the client metadata.
+   */
   const loginByPairingCode = useCallback(
     async (code: string) => {
-      return withAccount<PairingCodeLoginResponseData>(async (account) => {
+      return withAccount<PairingCodeLoginClientMetadata>(async (account) => {
         const { issuer, clientID } = account
         const { fcmDeviceToken, apnsToken } = await getNotificationTokens()
         const signedCode = await signPairingCode(code, issuer, clientID, fcmDeviceToken, apnsToken)
-        await apiClient.post<PairingCodeLoginResponseData>(
+        const response = await apiClient.post<PairingCodeLoginClientMetadata>(
           // this endpoint is not available through the .well-known/openid-configuration so it needs to be hardcoded
           `${apiClient.baseURL}/cardtap/v3/mobile/assertion`,
           { assertion: signedCode },
           { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
         )
-        return { success: true }
+
+        return response.data
       })
     },
     [apiClient]
