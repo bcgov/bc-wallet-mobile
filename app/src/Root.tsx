9985ba3fd68eeab4f75c5f35427c88800a5a25b7
Mac Deluca <Mac.Deluca@quartech.com>
2025-09-25 12:31:38 -0700

    fix: moved BCSCApiClientProvider to only wrap BCSC
    Signed-off-by: Mac Deluca <Mac.Deluca@quartech.com>

--- app/src/Root.tsx
+++ app/src/Root.tsx
@@ -1,11 +1,18 @@
 import { RootStack as BCWalletRootStack, useStore } from '@bifold/core'
 import { BCState, Mode } from '@/store'
 import BCSCRootStack from '@bcsc-theme/navigators/RootStack'
+import { BCSCApiClientProvider } from './bcsc-theme/contexts/BCSCApiClientContext'
 
 const Root: React.FC = () => {
   const [store] = useStore<BCState>()
 
-  return store.mode === Mode.BCSC ? <BCSCRootStack /> : <BCWalletRootStack />
+  return store.mode === Mode.BCSC ? (
+    <BCSCApiClientProvider>
+      <BCSCRootStack />
+    </BCSCApiClientProvider>
+  ) : (
+    <BCWalletRootStack />
+  )
 }
 
 export default Root
