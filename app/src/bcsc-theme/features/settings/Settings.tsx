ac85e01838e76440b5d8abfaedfb9a7d595eb526
Mac Deluca <Mac.Deluca@quartech.com>
2025-10-06 14:14:05 -0700

    feat: added navigation for biometry and pin Bifold screens
    Signed-off-by: Mac Deluca <Mac.Deluca@quartech.com>

--- app/src/bcsc-theme/features/settings/Settings.tsx
+++ app/src/bcsc-theme/features/settings/Settings.tsx
@@ -1,38 +1,27 @@
-import { UserInfoResponseData } from '@/bcsc-theme/api/hooks/useUserApi'
-import { BCThemeNames } from '@/constants'
-import { BCDispatchAction, BCState, Mode } from '@/store'
-import useApi from '@bcsc-theme/api/hooks/useApi'
-import { ServerStatusResponseData, TermsOfUseResponseData } from '@bcsc-theme/api/hooks/useConfigApi'
+import { BCState } from '@/store'
 import TabScreenWrapper from '@bcsc-theme/components/TabScreenWrapper'
-import useDataLoader from '@bcsc-theme/hooks/useDataLoader'
-import {
-  Button,
-  ButtonType,
-  isBiometricsActive,
-  LockoutReason,
-  ThemedText,
-  TOKENS,
-  useAuth,
-  useServices,
-  useStore,
-  useTheme,
-} from '@bifold/core'
+import { LockoutReason, ThemedText, TOKENS, useAuth, useServices, useStore, useTheme } from '@bifold/core'
 import React from 'react'
-import { ScrollView, StyleSheet, Text, View } from 'react-native'
-import SampleApiDisplay from './components/SampleApiDisplay'
+import { StyleSheet, View } from 'react-native'
 import { SettingsActionCard } from './components/SettingsActionCard'
 import { useTranslation } from 'react-i18next'
 import Icon from 'react-native-vector-icons/MaterialIcons'
-import { getBuildNumber, getSystemName, getSystemVersion, getVersion } from 'react-native-device-info'
+import { getBuildNumber, getVersion } from 'react-native-device-info'
+import { useNavigation } from '@react-navigation/native'
+import { StackNavigationProp } from '@react-navigation/stack'
+import { BCSCRootStackParams, BCSCScreens, BCSCStacks } from '@/bcsc-theme/types/navigators'
 
-// Placeholder for now, not sure if we want to reuse our
-// existing settings screen or create a new one, prob create new
+/**
+ * The Settings screen for the BCSC theme.
+ *
+ * @returns {*} {JSX.Element}
+ */
 const Settings: React.FC = () => {
   const { t } = useTranslation()
-  const { Spacing, setTheme, themeName, ColorPalette } = useTheme()
-  const [store, dispatch] = useStore<BCState>()
+  const navigation = useNavigation<StackNavigationProp<BCSCRootStackParams>>()
+  const { Spacing, ColorPalette } = useTheme()
+  const [store] = useStore<BCState>()
   const auth = useAuth()
-  const { config, evidence, user } = useApi()
   const [logger] = useServices([TOKENS.UTIL_LOGGER])
 
   const styles = StyleSheet.create({
@@ -59,72 +48,6 @@ const Settings: React.FC = () => {
     },
   })
 
-  const onServerStatusError = (error: unknown) => {
-    logger.error(`Error loading server status: ${error}`)
-  }
-  const onTermsOfUseError = (error: unknown) => {
-    logger.error(`Error loading terms of use: ${error}`)
-  }
-
-  const isBiometricsActive = useDataLoader(auth.isBiometricsActive, {
-    onError: (error: unknown) => logger.error(`Error loading biometrics: ${error}`),
-  })
-
-  const serverStatusDataLoader = useDataLoader<ServerStatusResponseData>(() => config.getServerStatus(), {
-    onError: onServerStatusError,
-  })
-  const termsDataLoader = useDataLoader<TermsOfUseResponseData>(() => config.getTermsOfUse(), {
-    onError: onTermsOfUseError,
-  })
-  const evidenceStart = useDataLoader<any>(
-    async () => {
-      if (!store.bcsc.deviceCode) {
-        throw new Error('Device code is not available. Something went wrong.')
-      }
-      // need to store id and SHA! for later
-      const response = await evidence.createVerificationRequest()
-      dispatch({ type: BCDispatchAction.UPDATE_VERIFICATION_REQUEST, payload: [response] })
-      return response
-    },
-    {
-      onError: (error: unknown) => logger.error(`Error loading: ${error}`),
-    }
-  )
-
-  const deleteRequest = useDataLoader<any>(
-    async () => {
-      if (store.bcsc.verificationRequestId && store.bcsc.deviceCode) {
-        await evidence.cancelVerificationRequest(store.bcsc.verificationRequestId)
-
-        return 'DELETED REQUEST FOR ID: ' + store.bcsc.verificationRequestId
-      } else {
-        return 'No ID found to delete'
-      }
-    },
-    {
-      onError: (error: unknown) => logger.error(`Error loading: ${error}`),
-    }
-  )
-
-  const userAccount = useDataLoader<any>(() => user.getUserInfo(), {
-    onError: (error: unknown) => logger.error(`Error loading: ${error}`),
-  })
-
-  const onPressMode = () => {
-    lockOutUser(LockoutReason.Logout)
-    const newTheme = store.mode === Mode.BCWallet ? BCThemeNames.BCSC : BCThemeNames.BCWallet
-    setTheme(newTheme)
-    dispatch({ type: BCDispatchAction.UPDATE_MODE, payload: [Mode.BCWallet] })
-  }
-
-  const onPressTheme = () => {
-    if (themeName === BCThemeNames.BCSC) {
-      setTheme(BCThemeNames.BCWallet)
-    } else {
-      setTheme(BCThemeNames.BCSC)
-    }
-  }
-
   // TODO (MD): Deprecate this once all settings actions have been implemented
   const onPressActionTodo = () => {
     logger.info('TODO: Settings action pressed')
@@ -133,7 +56,7 @@ const Settings: React.FC = () => {
   return (
     <TabScreenWrapper>
       <View style={styles.container}>
-        <ScrollView style={styles.sectionContainer}>
+        <View style={styles.sectionContainer}>
           <SettingsActionCard
             title={t('BCSCSettings.SignOut')}
             startAdornment={<Icon name="logout" size={24} color={ColorPalette.brand.secondary} />}
@@ -147,10 +70,19 @@ const Settings: React.FC = () => {
           {/* TODO (MD): Implement actions for these cards */}
           <SettingsActionCard
             title={t('BCSCSettings.Biometrics')}
-            onPress={onPressActionTodo}
-            endAdornmentText={isBiometricsActive.data ? 'OFF' : 'ON'}
+            onPress={() => {
+              // TODO (MD): Biometrics content from Bifold is specific to BC Wallet, we need to make dynamic for BCSC
+              navigation.navigate(BCSCStacks.SettingStack, { screen: BCSCScreens.ToggleBiometrics })
+            }}
+            endAdornmentText={store.preferences.useBiometry ? 'ON' : 'OFF'}
           />
-          <SettingsActionCard title={t('BCSCSettings.ChangePIN')} onPress={onPressActionTodo} />
+          <SettingsActionCard
+            title={t('BCSCSettings.ChangePIN')}
+            onPress={() => {
+              // TODO (MD): Bifold change PIN is rerouting to the BC Wallet settings screen, should go to previous screen instead
+              navigation.navigate(BCSCStacks.SettingStack, { screen: BCSCScreens.ChangePIN })
+            }}
+          />
           <SettingsActionCard
             title={t('BCSCSettings.AutoLockTime')}
             onPress={onPressActionTodo}
@@ -169,11 +101,10 @@ const Settings: React.FC = () => {
           <SettingsActionCard title={t('BCSCSettings.TermsOfUse')} onPress={onPressActionTodo} />
 
           <View style={styles.versionContainer}>
-            <ThemedText>BC Services Card</ThemedText>
-            <ThemedText>{`Version: ${getVersion()}`}</ThemedText>
-            <ThemedText>{`Build: ${getBuildNumber()}`}</ThemedText>
+            <ThemedText variant="labelSubtitle">BC Services Card</ThemedText>
+            <ThemedText variant="labelSubtitle">{`Version ${getVersion()}-${getBuildNumber()}`}</ThemedText>
           </View>
-        </ScrollView>
+        </View>
       </View>
     </TabScreenWrapper>
   )
