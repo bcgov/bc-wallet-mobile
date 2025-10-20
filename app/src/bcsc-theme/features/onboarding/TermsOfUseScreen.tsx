1383807d50935c03f9fc580fa53119e044afd7a3
Mac Deluca <Mac.Deluca@quartech.com>
2025-10-20 15:24:22 -0700

    chore: refactored existing onboarding screens to use WorkflowEngine controls
    Signed-off-by: Mac Deluca <Mac.Deluca@quartech.com>

--- app/src/bcsc-theme/features/onboarding/TermsOfUseScreen.tsx
+++ app/src/bcsc-theme/features/onboarding/TermsOfUseScreen.tsx
@@ -1,6 +1,5 @@
-import { BCSCOnboardingStackParams, BCSCScreens } from '@/bcsc-theme/types/navigators'
+import { useWorkflowEngine } from '@/contexts/WorkflowEngineContext'
 import { Button, ButtonType, testIdWithKey, ThemedText, useTheme } from '@bifold/core'
-import { StackNavigationProp } from '@react-navigation/stack'
 import { useTranslation } from 'react-i18next'
 import { ScrollView, StyleSheet, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
@@ -9,18 +8,15 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 const mockTermsOfUseContent =
   'Lorem ipsum dolor sit amet consectetur adipiscing elit quisque faucibus ex sapien vitae pellentesque sem placerat in id cursus mi pretium tellus duis convallis tempus.'
 
-interface TermsOfUseScreenProps {
-  navigation: StackNavigationProp<BCSCOnboardingStackParams, BCSCScreens.OnboardingTermsOfUseScreen>
-}
-
 /**
  * Terms of Use screen component that presents the application's terms of use to the user.
  *
  * @returns {*} {JSX.Element} The TermsOfUseScreen component.
  */
-export const TermsOfUseScreen = (props: TermsOfUseScreenProps): JSX.Element => {
+export const TermsOfUseScreen = (): JSX.Element => {
   const { t } = useTranslation()
   const theme = useTheme()
+  const workflowEngine = useWorkflowEngine()
 
   const styles = StyleSheet.create({
     container: {
@@ -53,7 +49,7 @@ export const TermsOfUseScreen = (props: TermsOfUseScreenProps): JSX.Element => {
           title={t('Unified.Onboarding.AcceptAndContinueButton')}
           buttonType={ButtonType.Primary}
           onPress={() => {
-            props.navigation.navigate(BCSCScreens.OnboardingNotificationsScreen)
+            workflowEngine.nextStep()
           }}
           testID={testIdWithKey('AcceptAndContinue')}
           accessibilityLabel={t('Unified.Onboarding.AcceptAndContinueButton')}
