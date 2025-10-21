7426abfd6901f37477242d28f527f06fd2fae941
Mac Deluca <Mac.Deluca@quartech.com>
2025-10-21 14:20:56 -0700

    doc: updated back header button jsdoc
    Signed-off-by: Mac Deluca <Mac.Deluca@quartech.com>

--- app/src/workflow/WorkflowEngineHeaderBackButton.tsx
+++ app/src/workflow/WorkflowEngineHeaderBackButton.tsx
@@ -7,6 +7,7 @@ import { useWorkflowEngine } from './useWorkflowEngine'
  *
  * Note: This is a curried function to avoid re-rendering in navigation stacks.
  *
+ * @returns {() => (props: HeaderBackButtonProps) => JSX.Element} A header back button component that navigates to the previous workflow step when pressed.
  */
 export const createWorkflowEngineBackHeaderButton = () => {
   // Declared so that it has a display name for debugging purposes
