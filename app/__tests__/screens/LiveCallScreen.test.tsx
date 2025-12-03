c302e1e7e591b0778af9dba507bc9772854d13d6
traviskhl <travislee2k15@gmail.com>
2025-12-03 15:19:21 -0800

    test: video call test
    Signed-off-by: traviskhl <travislee2k15@gmail.com>

---
+++ app/__tests__/screens/LiveCallScreen.test.tsx
@@ -0,0 +1,27 @@
+import { render } from '@testing-library/react-native'
+import React from 'react'
+
+import { useNavigation } from '../../__mocks__/custom/@react-navigation/core'
+import { BasicAppContext } from '../../__mocks__/helpers/app'
+import LiveCallScreen from '../../src/bcsc-theme/features/verify/live-call/LiveCallScreen'
+
+
+
+describe('LiveCall', () => {
+  let mockNavigation: any
+
+  beforeEach(() => {
+    mockNavigation = useNavigation()
+    jest.clearAllMocks()
+  })
+
+  it('renders correctly', () => {
+    const tree = render(
+      <BasicAppContext>
+        <LiveCallScreen navigation={mockNavigation as never} />
+      </BasicAppContext>
+    )
+
+    expect(tree).toMatchSnapshot()
+  })
+})
