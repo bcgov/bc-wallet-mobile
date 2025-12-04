7833df4d480d22aebe82ba826b9785ab12305e6b
Mac Deluca <99926243+MacDeluca@users.noreply.github.com>
2025-12-03 16:03:26 -0800

    fix: disable hardware back actions for send video success screen (#3006)
    Signed-off-by: Mac Deluca <Mac.Deluca@quartech.com>

--- app/src/bcsc-theme/navigators/VerifyStack.tsx
+++ app/src/bcsc-theme/navigators/VerifyStack.tsx
@@ -182,7 +182,7 @@ const VerifyStack = () => {
       <Stack.Screen
         name={BCSCScreens.SuccessfullySent}
         component={SuccessfullySentScreen}
-        options={{ headerShown: false }}
+        options={{ headerShown: false, gestureEnabled: false }}
       />
       <Stack.Screen
         name={BCSCScreens.VerificationSuccess}
