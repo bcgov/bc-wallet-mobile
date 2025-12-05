d4d86199097913b56561052cb3f4d5f777c85537
Alfred Rosenthal <alfred.k.rosenthal@gmail.com>
2025-12-05 14:51:14 -0800

    fix: wip
    Signed-off-by: Alfred Rosenthal <alfred.k.rosenthal@gmail.com>

--- app/__tests__/bcsc-theme/components/SettingsHeaderButton.test.tsx
+++ app/__tests__/bcsc-theme/components/SettingsHeaderButton.test.tsx
@@ -4,8 +4,21 @@ import {
 } from '@/bcsc-theme/components/SettingsHeaderButton'
 import { BCSCScreens } from '@/bcsc-theme/types/navigators'
 import { testIdWithKey } from '@bifold/core'
-import { useNavigation } from '@react-navigation/native'
 import { fireEvent, render } from '@testing-library/react-native'
+import { BasicAppContext } from '../../../__mocks__/helpers/app'
+
+// Mock useNavigation
+jest.mock('@react-navigation/native', () => {
+  const actual = jest.requireActual('@react-navigation/native')
+  return {
+    ...actual,
+    useNavigation: jest.fn(() => ({
+      navigate: jest.fn(),
+    })),
+  }
+})
+
+import { useNavigation } from '@react-navigation/native'
 
 describe('MainSettingsHeaderButton', () => {
   beforeEach(() => {
@@ -15,22 +28,32 @@ describe('MainSettingsHeaderButton', () => {
   it('renders the main settings menu button', () => {
     const MainSettingsHeaderButton = createMainSettingsHeaderButton()
 
-    const { getByTestId } = render(<MainSettingsHeaderButton />)
+    const { getByTestId } = render(
+      <BasicAppContext>
+        <MainSettingsHeaderButton />
+      </BasicAppContext>
+    )
 
     expect(getByTestId(testIdWithKey('SettingsMenuButton'))).toBeTruthy()
   })
 
   it('should navigate to MainSettings screen on press', async () => {
     const MainSettingsHeaderButton = createMainSettingsHeaderButton()
-    const navigation = useNavigation()
+    const mockNavigate = jest.fn()
+    ;(useNavigation as jest.Mock).mockReturnValue({
+      navigate: mockNavigate,
+    })
 
-    const { getByTestId } = render(<MainSettingsHeaderButton />)
+    const { getByTestId } = render(
+      <BasicAppContext>
+        <MainSettingsHeaderButton />
+      </BasicAppContext>
+    )
 
     const button = getByTestId(testIdWithKey('SettingsMenuButton'))
-
     fireEvent.press(button)
 
-    expect(navigation.navigate).toHaveBeenCalledWith(BCSCScreens.MainSettings)
+    expect(mockNavigate).toHaveBeenCalledWith(BCSCScreens.MainSettings)
   })
 })
 
@@ -42,21 +65,31 @@ describe('VerifySettingsHeaderButton', () => {
   it('renders the verify settings menu button', () => {
     const VerifySettingsHeaderButton = createVerifySettingsHeaderButton()
 
-    const { getByTestId } = render(<VerifySettingsHeaderButton />)
+    const { getByTestId } = render(
+      <BasicAppContext>
+        <VerifySettingsHeaderButton />
+      </BasicAppContext>
+    )
 
     expect(getByTestId(testIdWithKey('SettingsMenuButton'))).toBeTruthy()
   })
 
   it('should navigate to VerifySettings screen on press', async () => {
     const VerifySettingsHeaderButton = createVerifySettingsHeaderButton()
-    const navigation = useNavigation()
+    const mockNavigate = jest.fn()
+    ;(useNavigation as jest.Mock).mockReturnValue({
+      navigate: mockNavigate,
+    })
 
-    const { getByTestId } = render(<VerifySettingsHeaderButton />)
+    const { getByTestId } = render(
+      <BasicAppContext>
+        <VerifySettingsHeaderButton />
+      </BasicAppContext>
+    )
 
     const button = getByTestId(testIdWithKey('SettingsMenuButton'))
-
     fireEvent.press(button)
 
-    expect(navigation.navigate).toHaveBeenCalledWith(BCSCScreens.VerifySettings)
+    expect(mockNavigate).toHaveBeenCalledWith(BCSCScreens.VerifySettings)
   })
 })
