470ab900dfbc537630950953e7fbce0248a56bda
Travis Lee <92693636+travkhl@users.noreply.github.com>
2025-12-12 11:11:10 -0800

    feat: add remove account button to transfer success screen (#3034)
    Signed-off-by: traviskhl <travislee2k15@gmail.com>

--- app/__tests__/screens/TransferSuccessScreen.test.tsx
+++ app/__tests__/screens/TransferSuccessScreen.test.tsx
@@ -1,11 +1,18 @@
-import { render } from '@testing-library/react-native'
+import { testIdWithKey } from '@bifold/core'
+import { fireEvent, render } from '@testing-library/react-native'
 import React from 'react'
 
+import { act } from 'react-test-renderer'
+import { useNavigation } from '../../__mocks__/@react-navigation/native'
 import { BasicAppContext } from '../../__mocks__/helpers/app'
 import TransferSuccessScreen from '../../src/bcsc-theme/features/account-transfer/TransferSuccessScreen'
+import { BCSCScreens, BCSCStacks } from '../../src/bcsc-theme/types/navigators'
 
 describe('TransferSuccess', () => {
+  let mockNavigation: any
+
   beforeEach(() => {
+    mockNavigation = useNavigation()
     jest.clearAllMocks()
     jest.useFakeTimers()
   })
@@ -14,13 +21,47 @@ describe('TransferSuccess', () => {
     jest.useRealTimers()
   })
 
-  it('renders correctly', () => {
-    const tree = render(
-      <BasicAppContext>
-        <TransferSuccessScreen />
-      </BasicAppContext>
-    )
+  describe('Render tests', () => {
+    it('renders correctly', () => {
+      const tree = render(
+        <BasicAppContext>
+          <TransferSuccessScreen />
+        </BasicAppContext>
+      )
 
-    expect(tree).toMatchSnapshot()
+      expect(tree).toMatchSnapshot()
+    })
+  })
+
+  describe('Navigation tests', () => {
+    it('navigates to Home when primary button is pressed', () => {
+      const { getByTestId } = render(
+        <BasicAppContext>
+          <TransferSuccessScreen />
+        </BasicAppContext>
+      )
+
+      const homeButton = getByTestId(testIdWithKey('BCSC.TransferSuccess.ButtonText'))
+      act(() => {
+        fireEvent.press(homeButton)
+      })
+
+      expect(mockNavigation.navigate).toHaveBeenCalledWith(BCSCStacks.Tab, { screen: BCSCScreens.Home })
+    })
+
+    it('navigates to RemoveAccountConfirmation when remove account button is pressed', () => {
+      const { getByTestId } = render(
+        <BasicAppContext>
+          <TransferSuccessScreen />
+        </BasicAppContext>
+      )
+
+      const removeAccountButton = getByTestId(testIdWithKey('BCSC.Account.RemoveAccount'))
+      act(() => {
+        fireEvent.press(removeAccountButton)
+      })
+
+      expect(mockNavigation.navigate).toHaveBeenCalledWith(BCSCScreens.RemoveAccountConfirmation)
+    })
   })
 })
