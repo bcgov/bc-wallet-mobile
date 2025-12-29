a273c7c26e866d78af8d4000b427199e7d1c256f
Alfred Rosenthal <alfred.k.rosenthal@gmail.com>
2025-12-29 11:29:27 -0800

    feat: wip, fixing qr transfer
    Signed-off-by: Alfred Rosenthal <alfred.k.rosenthal@gmail.com>

--- app/src/bcsc-theme/features/account-transfer/transferee/TransferQRScannerScreen.tsx
+++ app/src/bcsc-theme/features/account-transfer/transferee/TransferQRScannerScreen.tsx
@@ -1,4 +1,6 @@
 import useApi from '@/bcsc-theme/api/hooks/useApi'
+import { useFactoryReset } from '@/bcsc-theme/api/hooks/useFactoryReset'
+import useRegistrationApi from '@/bcsc-theme/api/hooks/useRegistrationApi'
 import { useBCSCApiClientState } from '@/bcsc-theme/hooks/useBCSCApiClient'
 import useSecureActions from '@/bcsc-theme/hooks/useSecureActions'
 import { BCSCOnboardingStackParams } from '@/bcsc-theme/types/navigators'
@@ -20,16 +22,19 @@ import { StackNavigationProp } from '@react-navigation/stack'
 import React, { useCallback, useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { ActivityIndicator, Alert, StyleSheet, View } from 'react-native'
-import { createDeviceSignedJWT, getAccount } from 'react-native-bcsc-core'
-import uuid from 'react-native-uuid'
+import { AccountSecurityMethod, getAccount } from 'react-native-bcsc-core'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import { useCameraPermission } from 'react-native-vision-camera'
 
+// TODO (Al): Need to run a factoryReset if the QR code fails
 const TransferQRScannerScreen: React.FC = () => {
   const { deviceAttestation, authorization, token } = useApi()
-  const { client } = useBCSCApiClientState()
+  const { client, isClientReady } = useBCSCApiClientState()
   const navigator = useNavigation<StackNavigationProp<BCSCOnboardingStackParams>>()
   const [store] = useStore<BCState>()
+  const reset = useFactoryReset()
+
+  const { register } = useRegistrationApi(client, isClientReady)
   const { ColorPalette, Spacing } = useTheme()
   const [isLoading, setIsLoading] = useState(false)
   const [scanError, setScanError] = useState<QrCodeScanError | null>(null)
@@ -100,6 +105,10 @@ const TransferQRScannerScreen: React.FC = () => {
     checkPermissions()
   }, [hasPermission, requestPermission, navigator, t])
 
+  useEffect(() => {
+    reset()
+  }, [])
+
   const handleScan = useCallback(
     async (value: string) => {
       // exit early if processing a scan already or if there's an error showing
@@ -109,63 +118,92 @@ const TransferQRScannerScreen: React.FC = () => {
 
       setIsLoading(true)
       setScanError(null)
+
+      /*
+      - this needs to register the device
+      - take the device code from that (save it) 
+      - use the client ID from the decoded QR code?
+      */
+
+      // TODO: (Al) Is there a security method for self transfer?
+      // TODO: (Al) This will need to clean up when this screen in loaded, remove anything and re-register
+      const registrationResults = await register(AccountSecurityMethod.)
+      let clientId = ''
+      // What happens if go back to this screen afterwards? like register, get an account, then close the app?
+      // do the registration results have what we need?
+      console.log('Registration Results:', registrationResults?.client_id)
       const account = await getAccount()
       if (!account) {
         setScanError(new QrCodeScanError(t('BCSC.Scan.InvalidQrCode'), value, t('BCSC.Scan.NoAccountFound')))
         return
       }
-      try {
-        const qrToken = value.split('?')[1]
-        // ias tokens expect times in seconds since epoch
-        const timeInSeconds = Math.floor(Date.now() / 1000)
+      clientId = account?.clientID
 
-        const jwt = await createDeviceSignedJWT({
-          aud: account.issuer,
-          iss: account.clientID,
-          sub: account.clientID,
-          iat: timeInSeconds,
-          exp: timeInSeconds + 60, // give this token 1 minute to live
-          jti: uuid.v4().toString(),
-        })
+      console.log('_____ CLIENT ID: ')
+      console.log(clientId)
 
-        if (store.bcscSecure.deviceCode) {
-          const response = await deviceAttestation.verifyAttestation({
-            client_id: account.clientID,
-            device_code: store.bcscSecure.deviceCode,
-            attestation: qrToken,
-            client_assertion: jwt,
-          })
 
-          if (!response) {
-            setScanError(new QrCodeScanError(t('BCSC.Scan.InvalidQrCode'), value, t('BCSC.Scan.NoAttestationResponse')))
-            return
-          }
 
-          const deviceToken = await token.deviceToken({
-            client_id: account.clientID,
-            device_code: store.bcscSecure.deviceCode,
-            client_assertion: jwt,
-          })
+      // console.log('Scanned QR Code:', account)
+      // try {
+      //   const qrToken = value.split('?')[1]
+      //   // ias tokens expect times in seconds since epoch
+      //   const timeInSeconds = Math.floor(Date.now() / 1000)
+      //   console.log('QR Token:', qrToken)
+      //   const decodedQR = jwtDecode(qrToken)
 
-          // api client needs the new tokens from IAS
-          if (client) {
-            client.tokens = deviceToken
-          }
+      //   const jwt = await createDeviceSignedJWT({
+      //     aud: decodedQR.aud,
+      //     iss: decodedQR.iss,
+      //     sub: decodedQR.sub,
+      //     iat: timeInSeconds,
+      //     exp: timeInSeconds + 60, // give this token 1 minute to live
+      //     jti: uuid.v4().toString(),
+      //   })
 
-          // TODO (bm): clean up old way below and implement nickname and app security selection
-          // flow after successful transfer, followed by account creation and login
+      //   console.log('________')
+      //   console.log(jwt)
+      //   console.log(store.bcscSecure.deviceCode)
+      //   if (store.bcscSecure.deviceCode) {
+      //     const response = await deviceAttestation.verifyAttestation({
+      //       client_id: decodedQR.iss,
+      //       device_code: store.bcscSecure.deviceCode,
+      //       attestation: qrToken,
+      //       client_assertion: jwt,
+      //     })
 
-          // OLD WAY - IGNORE
-          // dispatch({ type: BCDispatchAction.UPDATE_REFRESH_TOKEN, payload: [deviceToken.refresh_token] })
-          // navigator.navigate(BCSCScreens.VerificationSuccess)
-        } else {
-          setScanError(new QrCodeScanError(t('BCSC.Scan.InvalidQrCode'), value, t('BCSC.Scan.NoDeviceCodeFound')))
-        }
-      } catch (error) {
-        setScanError(new QrCodeScanError(t('BCSC.Scan.InvalidQrCode'), value, (error as Error)?.message))
-      } finally {
-        setIsLoading(false)
-      }
+      //     if (!response) {
+      //       setScanError(new QrCodeScanError(t('BCSC.Scan.InvalidQrCode'), value, t('BCSC.Scan.NoAttestationResponse')))
+      //       return
+      //     }
+
+      //     const deviceToken = await token.deviceToken({
+      //       client_id: decodedQR.iss,
+      //       device_code: store.bcscSecure.deviceCode,
+      //       client_assertion: jwt,
+      //     })
+      //     console.log('______________')
+      //     console.log('______________')
+      //     console.log('______________')
+      //     console.log(store.bcscSecure.deviceCode)
+      //     console.log(deviceToken)
+      //     // api client needs the new tokens from IAS
+      //     if (client) {
+      //       client.tokens = deviceToken
+      //     }
+
+      //     // TODO (bm): clean up old way below and implement nickname and app security selection
+      //     // flow after successful transfer, followed by account creation and login
+
+      //     // I wonder how this will change things...
+      //   } else {
+      //     setScanError(new QrCodeScanError(t('BCSC.Scan.InvalidQrCode'), value, t('BCSC.Scan.NoDeviceCodeFound')))
+      //   }
+      // } catch (error) {
+      //   setScanError(new QrCodeScanError(t('BCSC.Scan.InvalidQrCode'), value, (error as Error)?.message))
+      // } finally {
+      //   setIsLoading(false)
+      // }
     },
     [store.bcscSecure.deviceCode, deviceAttestation, client, t, token, isLoading, scanError]
   )
