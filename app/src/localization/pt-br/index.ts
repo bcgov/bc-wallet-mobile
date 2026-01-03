202381bf69e9ce63204f586a01cef3b5fcddaeee
Matthew <100318133+MKGpp@users.noreply.github.com>
2026-01-02 16:07:20 -0800

    fix: remove from other device does not actually remove mobile card from other device (#3064)
    Signed-off-by: matthew <matthew.golshani@gov.bc.ca>
    Signed-off-by: Bryce McMath <bryce.j.mcmath@gmail.com>
    Co-authored-by: matthew <matthew.golshani@gov.bc.ca>
    Co-authored-by: Bryce McMath <bryce.j.mcmath@gmail.com>

--- app/src/localization/pt-br/index.ts
+++ app/src/localization/pt-br/index.ts
@@ -197,8 +197,9 @@ const translation = {
       },
       "DeviceInvalidated": {
         "Header": "Device invalidated (PT-BR)",
-        "ContentA": "This device has been invalidated. You must re-authorize the device to continue. (PT-BR)",
-        "ContentB": "Tap OK to clear local data and restart setup on this device. (PT-BR)",
+        "CancelledByAgent": "This device has been invalidated. You must re-authorize the device to continue. (PT-BR)",
+        "CancelledByUser": "This device has been removed from your account by a user action. (PT-BR)",
+        "ContentA": "Tap OK to clear local data and restart setup on this device. (PT-BR)",
         "OKButton": "OK (PT-BR)",
       }  
     },
