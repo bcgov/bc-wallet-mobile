1e8cfb6af396cdd6c9d3f4bd8ff7cd221a285666
Jason C. Leach <jason.leach@fullboar.ca>
2026-01-15 15:53:58 -0800

    fix: forgot to add mock
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- app/__mocks__/@react-native-firebase/messaging.ts
+++ app/__mocks__/@react-native-firebase/messaging.ts
@@ -1,6 +1,8 @@
 const messaging = jest.fn().mockReturnValue({
   setBackgroundMessageHandler: jest.fn(),
   onMessage: jest.fn(),
+  onNotificationOpenedApp: jest.fn(),
+  getInitialNotification: jest.fn().mockResolvedValue(null),
   requestPermission: jest.fn(),
 })
 
