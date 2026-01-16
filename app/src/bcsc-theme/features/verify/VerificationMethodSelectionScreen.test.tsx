e48359247d4c3f64cad7bb3cf0189b3b3780b60a
Bryce McMath <32586431+bryce-mcmath@users.noreply.github.com>
2026-01-16 11:42:28 -0800

    fix: verify options ordering, heading, and spacing (#3163)
    Signed-off-by: Bryce McMath <bryce.j.mcmath@gmail.com>

--- app/src/bcsc-theme/features/verify/VerificationMethodSelectionScreen.test.tsx
+++ app/src/bcsc-theme/features/verify/VerificationMethodSelectionScreen.test.tsx
@@ -1,12 +1,18 @@
+import { DeviceVerificationOption } from '@/bcsc-theme/api/hooks/useAuthorizationApi'
 import { render } from '@testing-library/react-native'
 import React from 'react'
 
 import { useNavigation } from '@mocks/custom/@react-navigation/core'
 import { BasicAppContext } from '@mocks/helpers/app'
+import useVerificationMethodModel from './_models/useVerificationMethodModel'
 import VerificationMethodSelectionScreen from './VerificationMethodSelectionScreen'
+
+jest.mock('./_models/useVerificationMethodModel')
 
 describe('VerificationMethodSelection', () => {
   let mockNavigation: any
+  const mockHandlePressSendVideo = jest.fn()
+  const mockHandlePressLiveCall = jest.fn()
 
   beforeEach(() => {
     mockNavigation = useNavigation()
@@ -18,7 +24,63 @@ describe('VerificationMethodSelection', () => {
     jest.useRealTimers()
   })
 
-  it('renders correctly', () => {
+  it('renders correctly with send_video as primary option', () => {
+    jest.mocked(useVerificationMethodModel).mockReturnValue({
+      handlePressSendVideo: mockHandlePressSendVideo,
+      handlePressLiveCall: mockHandlePressLiveCall,
+      sendVideoLoading: false,
+      liveCallLoading: false,
+      verificationOptions: [
+        DeviceVerificationOption.SEND_VIDEO,
+        DeviceVerificationOption.LIVE_VIDEO_CALL,
+        DeviceVerificationOption.IN_PERSON,
+      ],
+    })
+
+    const tree = render(
+      <BasicAppContext>
+        <VerificationMethodSelectionScreen navigation={mockNavigation as never} />
+      </BasicAppContext>
+    )
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders correctly with video_call as primary option', () => {
+    jest.mocked(useVerificationMethodModel).mockReturnValue({
+      handlePressSendVideo: mockHandlePressSendVideo,
+      handlePressLiveCall: mockHandlePressLiveCall,
+      sendVideoLoading: false,
+      liveCallLoading: false,
+      verificationOptions: [
+        DeviceVerificationOption.LIVE_VIDEO_CALL,
+        DeviceVerificationOption.SEND_VIDEO,
+        DeviceVerificationOption.IN_PERSON,
+      ],
+    })
+
+    const tree = render(
+      <BasicAppContext>
+        <VerificationMethodSelectionScreen navigation={mockNavigation as never} />
+      </BasicAppContext>
+    )
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders correctly with in_person as primary option', () => {
+    jest.mocked(useVerificationMethodModel).mockReturnValue({
+      handlePressSendVideo: mockHandlePressSendVideo,
+      handlePressLiveCall: mockHandlePressLiveCall,
+      sendVideoLoading: false,
+      liveCallLoading: false,
+      verificationOptions: [
+        DeviceVerificationOption.IN_PERSON,
+        DeviceVerificationOption.SEND_VIDEO,
+        DeviceVerificationOption.LIVE_VIDEO_CALL,
+      ],
+    })
+
     const tree = render(
       <BasicAppContext>
         <VerificationMethodSelectionScreen navigation={mockNavigation as never} />
