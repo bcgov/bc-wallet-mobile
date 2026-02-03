cf3df3bae3eaf0641a465ca107042c01dd8685cb
Bryce McMath <32586431+bryce-mcmath@users.noreply.github.com>
2026-02-03 12:13:54 -0800

    feat(deps): 16kb requirement (#3229)
    Signed-off-by: Bryce McMath <bryce.j.mcmath@gmail.com>
    Co-authored-by: Al Rosenthal <alfred.k.rosenthal@gmail.com>

--- app/index.js
+++ app/index.js
@@ -1,23 +1,6 @@
 // organize-imports-ignore
 import 'fast-text-encoding' // polyfill for TextEncoder and TextDecoder
 import 'react-native-gesture-handler'
-import '@formatjs/intl-getcanonicallocales/polyfill'
-import '@formatjs/intl-locale/polyfill'
-import '@formatjs/intl-pluralrules/polyfill'
-import '@formatjs/intl-pluralrules/locale-data/en' // locale-data for en
-import '@formatjs/intl-displaynames/polyfill'
-import '@formatjs/intl-displaynames/locale-data/en' // locale-data for en
-import '@formatjs/intl-listformat/polyfill'
-import '@formatjs/intl-listformat/locale-data/en' // locale-data for en
-import '@formatjs/intl-numberformat/polyfill'
-import '@formatjs/intl-numberformat/locale-data/en' // locale-data for en
-import '@formatjs/intl-relativetimeformat/polyfill'
-import '@formatjs/intl-relativetimeformat/locale-data/en' // locale-data for en
-import '@formatjs/intl-datetimeformat/polyfill'
-import '@formatjs/intl-datetimeformat/locale-data/en' // locale-data for en
-import '@formatjs/intl-datetimeformat/locale-data/fr' // locale-data for fr
-import '@formatjs/intl-datetimeformat/locale-data/pt' // locale-data for pt-BR
-import '@formatjs/intl-datetimeformat/add-all-tz' // Add ALL tz data
 import 'reflect-metadata'
 
 import { decode, encode } from 'base-64'
@@ -28,6 +11,12 @@ if (!global.btoa) {
 
 if (!global.atob) {
   global.atob = decode
+}
+
+import { Buffer } from 'buffer'
+
+if (typeof global.Buffer === 'undefined') {
+  global.Buffer = Buffer
 }
 
 import { AppRegistry, LogBox } from 'react-native'
