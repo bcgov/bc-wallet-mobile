6f1e58a98b77a0ab6a9591c2fcb024d965720f97
Mac Deluca <99926243+MacDeluca@users.noreply.github.com>
2026-02-26 20:11:19 -0800

    feat: reset id collection setup step 2 (#3323)
    Signed-off-by: Mac Deluca <Mac.Deluca@quartech.com>

--- app/src/localization/pt-br/index.ts
+++ app/src/localization/pt-br/index.ts
@@ -1147,6 +1147,11 @@ const translation = {
       "Title": "Problem with Connection (PT-BR)",
       "Description": "Please try again. (PT-BR)"
     },
+    "FactoryReset": {
+      "Title": "Problem with App (PT-BR)",
+      "Description": "The app needs to be reset to factory settings to continue. This will delete all information in the app and you will have to set it up again. (PT-BR)",
+      "Action1": "Reset App (PT-BR)"
+    }
 	},
   "BCWalletError": {
     // TODO (MD): Fill in translations once all english errors are completed
