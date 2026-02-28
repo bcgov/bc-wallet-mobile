84473e9c22b0d3866fa7c0da8feee177736f2846
Bryce McMath <bryce.j.mcmath@gmail.com>
2026-02-28 13:34:02 -0800

    fix: handle mononyms
    Signed-off-by: Bryce McMath <bryce.j.mcmath@gmail.com>

--- app/src/bcsc-theme/features/account/Account.tsx
+++ app/src/bcsc-theme/features/account/Account.tsx
@@ -142,7 +142,9 @@ const Account: React.FC = () => {
           {/*TODO (MD): fallback for when this is undefined (silhouette) */}
           <AccountPhoto photoUri={account.picture} />
           <ThemedText variant={'headingTwo'} style={styles.name}>
-            {account.family_name}, {account.given_name}
+            {account.given_name && account.family_name
+              ? `${account.family_name}, ${account.given_name}`
+              : account.family_name || account.given_name || ''}
           </ThemedText>
         </View>
         <ThemedText style={styles.warning}>{t('BCSC.Account.AccountInfo.Description')}</ThemedText>
