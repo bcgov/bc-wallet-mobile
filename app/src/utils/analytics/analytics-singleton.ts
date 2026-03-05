38f23a9d89c088776dda8323d1882e55fc39a38e
Mac Deluca <Mac.Deluca@quartech.com>
2026-03-05 13:49:08 -0800

    fix: added flag for analytics
    Signed-off-by: Mac Deluca <Mac.Deluca@quartech.com>

--- app/src/utils/analytics/analytics-singleton.ts
+++ app/src/utils/analytics/analytics-singleton.ts
@@ -6,7 +6,22 @@ declare global {
   var __ANALYTICS_TRACKER_SINGLETON__: AnalyticsTracker | undefined
 }
 
+// NOTE: This allows the CI/CD pipeline to be plumbed together and Github variable added (Github doesn't allow empty strings for variables)
+const FLAG_DISABLE_ANALYTICS = 'FLAG_DISABLE_ANALYTICS'
+
+const getSnowplowEndpointUrl = (): string => {
+  if (Config.SNOWPLOW_COLLECTOR_URL === FLAG_DISABLE_ANALYTICS) {
+    return ''
+  }
+
+  if (!Config.SNOWPLOW_COLLECTOR_URL) {
+    return ''
+  }
+
+  return Config.SNOWPLOW_COLLECTOR_URL
+}
+
 // Hot-reload safe singleton instance of AnalyticsTracker
 export const Analytics =
   globalThis.__ANALYTICS_TRACKER_SINGLETON__ ??
-  (globalThis.__ANALYTICS_TRACKER_SINGLETON__ = new AnalyticsTracker(Config.SNOWPLOW_COLLECTOR_URL ?? ''))
+  (globalThis.__ANALYTICS_TRACKER_SINGLETON__ = new AnalyticsTracker(getSnowplowEndpointUrl()))
