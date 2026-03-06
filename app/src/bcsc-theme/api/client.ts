eb0aa8fed31bb85f2fcda69586933608b4cd6233
Jason C. Leach <jason.leach@fullboar.ca>
2026-03-06 09:17:00 -0800

    fix: format errors or warnings
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- app/src/bcsc-theme/api/client.ts
+++ app/src/bcsc-theme/api/client.ts
@@ -1,10 +1,10 @@
+import { AppError } from '@/errors/appError'
+import { ErrorRegistry } from '@/errors/errorRegistry'
 import { RemoteLogger } from '@bifold/remote-logs'
 import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse, InternalAxiosRequestConfig } from 'axios'
 import { jwtDecode } from 'jwt-decode'
 import merge from 'lodash.merge'
 import { getRefreshTokenRequestBody } from 'react-native-bcsc-core'
-import { AppError } from '@/errors/appError'
-import { ErrorRegistry } from '@/errors/errorRegistry'
 import {
   formatAxiosErrorForLogger as formatIASAxiosErrorForLogger,
   formatIasAxiosResponseError,
