1018a23ac9a4141779a0af5024ed7e795fbb9747
Alfred Rosenthal <alfred.k.rosenthal@gmail.com>
2026-03-09 20:33:07 -0700

    chore: added test ids and updated snapshots
    Signed-off-by: Alfred Rosenthal <alfred.k.rosenthal@gmail.com>

--- app/src/bcsc-theme/features/verify/live-call/components/ServicePeriodList.tsx
+++ app/src/bcsc-theme/features/verify/live-call/components/ServicePeriodList.tsx
@@ -1,6 +1,7 @@
 import { FormattedServicePeriod } from '@/bcsc-theme/utils/service-hours-formatter'
-import { ThemedText } from '@bifold/core'
+import { testIdWithKey, ThemedText } from '@bifold/core'
 import { useTranslation } from 'react-i18next'
+import { View } from 'react-native'
 import ServicePeriod from './ServicePeriod'
 
 type ServicePeriodListProps = {
@@ -9,14 +10,14 @@ type ServicePeriodListProps = {
 const ServicePeriodList = ({ items }: ServicePeriodListProps) => {
   const { t } = useTranslation()
   if (!items.length) {
-    return <ThemedText>{t('BCSC.VideoCall.DefaultHours')}</ThemedText>
+    return <ThemedText testID={testIdWithKey('ServiceHours')}>{t('BCSC.VideoCall.DefaultHours')}</ThemedText>
   }
   return (
-    <>
+    <View testID={testIdWithKey('BeforeYouCallTitle')}>
       {items.map((item) => (
         <ServicePeriod servicePeriod={item} key={`${JSON.stringify(item)}`} />
       ))}
-    </>
+    </View>
   )
 }
 
