9ebe4b9fadc4921141421eeac15d64669108d1c0
Mac Deluca <Mac.Deluca@quartech.com>
2026-03-11 12:02:01 -0700

    chore: updated rootstack and useSecureAtions
    Signed-off-by: Mac Deluca <Mac.Deluca@quartech.com>

--- app/src/bcsc-theme/navigators/RootStack.tsx
+++ app/src/bcsc-theme/navigators/RootStack.tsx
@@ -1,15 +1,15 @@
 import { useErrorAlert } from '@/contexts/ErrorAlertContext'
 import { useNavigationContainer } from '@/contexts/NavigationContainerContext'
-import { BCDispatchAction, BCState } from '@/store'
+import { BCState } from '@/store'
 import { TOKENS, useServices, useStore } from '@bifold/core'
 import React, { useEffect, useState } from 'react'
-import { getAccount } from 'react-native-bcsc-core'
 import useThirdPartyKeyboardWarning from '../api/hooks/useThirdPartyKeyboardWarning'
 import { BCSCAccountProvider } from '../contexts/BCSCAccountContext'
 import { BCSCActivityProvider } from '../contexts/BCSCActivityContext'
 import { BCSCIdTokenProvider } from '../contexts/BCSCIdTokenContext'
 import { LoadingScreen } from '../contexts/BCSCLoadingContext'
 import { useBCSCApiClientState } from '../hooks/useBCSCApiClient'
+import useSecureActions from '../hooks/useSecureActions'
 import { SystemCheckScope, useSystemChecks } from '../hooks/useSystemChecks'
 import AuthStack from './AuthStack'
 import BCSCMainStack from './MainStack'
@@ -21,9 +21,9 @@ const BCSCRootStack: React.FC = () => {
   const [loadState] = useServices([TOKENS.LOAD_STATE])
   const { isClientReady } = useBCSCApiClientState()
   const [loading, setLoading] = useState(true)
-  const [logger] = useServices([TOKENS.UTIL_LOGGER])
   const { emitErrorModal } = useErrorAlert()
   const { isNavigationReady } = useNavigationContainer()
+  const { hydrateAccount } = useSecureActions()
   useSystemChecks(SystemCheckScope.STARTUP)
   useThirdPartyKeyboardWarning()
 
@@ -40,32 +40,19 @@ const BCSCRootStack: React.FC = () => {
     }
   }, [dispatch, loadState, emitErrorModal, store.stateLoaded])
 
-  // Check for existing account on initial load - only runs after state is loaded
-  // FIXME: This logic appears slightly too complex for the RootStack
-  // consider moving to a system check ie: V3AccountRequiresMigrationSystemCheck
   useEffect(() => {
     if (!store.stateLoaded || !loading) {
       return
     }
 
     const asyncEffect = async () => {
-      try {
-        const account = await getAccount()
-
-        dispatch({ type: BCDispatchAction.SET_HAS_ACCOUNT, payload: [Boolean(account)] })
-
-        if (account?.nickname && !store.bcsc.nicknames.includes(account.nickname)) {
-          dispatch({ type: BCDispatchAction.ADD_NICKNAME, payload: [account.nickname] })
-        }
-      } catch (error) {
-        logger.error('Error checking for existing account:', error as Error)
-        dispatch({ type: BCDispatchAction.SET_HAS_ACCOUNT, payload: [false] })
-      } finally {
-        setLoading(false)
-      }
+      await hydrateAccount()
+      setLoading(false)
     }
+
     asyncEffect()
-  }, [dispatch, loading, logger, store.bcsc.nicknames, store.stateLoaded])
+  }, [hydrateAccount, loading, store.stateLoaded])
+  //
 
   // Show loading screen if state, API client or navigation is not ready
   if (!store.stateLoaded || !isClientReady || loading || !isNavigationReady) {
---
+++ app/src/bcsc-theme/utils/retry.test.ts
@@ -0,0 +1,77 @@
+import { retryAsync } from './retry'
+
+describe('retryAsync', () => {
+  it('attempts the request the specified number of times', async () => {
+    const fn = jest.fn().mockRejectedValue(new Error('Failed'))
+
+    try {
+      await retryAsync(fn, 3, 100)
+    } catch (error) {
+      expect(error).toEqual(new Error('Failed'))
+      expect(fn).toHaveBeenCalledTimes(3)
+    }
+  })
+
+  it('succeeds if the request eventually succeeds', async () => {
+    const fn = jest.fn().mockRejectedValueOnce(new Error('Failed')).mockResolvedValueOnce('Success')
+
+    const result = await retryAsync(fn, 3, 100)
+
+    expect(result).toBe('Success')
+  })
+
+  it('throws an error if the request fails after all retries', async () => {
+    const fn = jest.fn().mockRejectedValue(new Error('Failed'))
+
+    try {
+      await retryAsync(fn, 2, 100)
+    } catch (error) {
+      expect(error).toEqual(new Error('Failed'))
+      expect(fn).toHaveBeenCalledTimes(2)
+    }
+  })
+
+  it('retrys if retryIfNullish is true and the result is null', async () => {
+    const fn = jest.fn().mockResolvedValueOnce(null).mockResolvedValueOnce('Success')
+
+    const result = await retryAsync(fn, 3, 100, true)
+
+    expect(result).toBe('Success')
+    expect(fn).toHaveBeenCalledTimes(2)
+  })
+
+  it('waits the specified delay between retries', async () => {
+    const fn = jest.fn().mockRejectedValue(new Error('Failed'))
+    const delay = 100
+    const startTime = Date.now()
+
+    try {
+      await retryAsync(fn, 3, delay)
+    } catch (error) {
+      const endTime = Date.now()
+      const elapsedTime = endTime - startTime
+      // Attempt 1: Immediate -> Attempt 2: After delay -> Attempt 3: After delay
+      expect(elapsedTime).toBeGreaterThanOrEqual(delay * 2)
+    }
+  })
+
+  it('throws an error if maxRetries is set to 0', async () => {
+    const fn = jest.fn().mockRejectedValue(new Error('Failed'))
+
+    try {
+      await retryAsync(fn, 0, 100)
+    } catch (error) {
+      expect(error).toEqual(new Error('[retryAsync]: attempts < 1'))
+    }
+  })
+
+  it('throws an error if maxRetries is negative', async () => {
+    const fn = jest.fn().mockRejectedValue(new Error('Failed'))
+
+    try {
+      await retryAsync(fn, -1, 100)
+    } catch (error) {
+      expect(error).toEqual(new Error('[retryAsync]: attempts < 1'))
+    }
+  })
+})
