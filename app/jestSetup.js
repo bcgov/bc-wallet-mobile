fceb6b04196bad0786b33adbf332dae4b6798be3
Jason C. Leach <jason.leach@fullboar.ca>
2026-03-18 14:53:09 -0700

    fix: use realistic device-info mock values in global test setup
    Update jestSetup.js to provide realistic react-native-device-info
    mock values (app name, version, build, OS) so snapshots render
    a proper user-agent string instead of "unknown/1".
    
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- app/jestSetup.js
+++ app/jestSetup.js
@@ -61,8 +61,11 @@ console.warn = createFilteredConsole('warn', [...suppressedPatterns, ...expected
 // eslint-disable-next-line no-console
 console.error = createFilteredConsole('error', [...suppressedPatterns, ...expectedReactWarnings])
 
-mockRNDeviceInfo.getVersion = jest.fn(() => '1')
-mockRNDeviceInfo.getBuildNumber = jest.fn(() => '1')
+mockRNDeviceInfo.getApplicationName = jest.fn(() => 'BCServicesCard')
+mockRNDeviceInfo.getVersion = jest.fn(() => '2.1.0')
+mockRNDeviceInfo.getBuildNumber = jest.fn(() => '142')
+mockRNDeviceInfo.getSystemName = jest.fn(() => 'iOS')
+mockRNDeviceInfo.getSystemVersion = jest.fn(() => '17.4')
 
 jest.mock('react-native-safe-area-context', () => mockSafeAreaContext)
 jest.mock('@react-native-community/netinfo', () => mockRNCNetInfo)
