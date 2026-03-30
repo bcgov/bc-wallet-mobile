d1323f0ce6d8461b321f989b304eedbf5ccac721
Alfred Rosenthal <alfred.k.rosenthal@gmail.com>
2026-03-30 15:20:48 -0700

    fix: fixed test
    Signed-off-by: Alfred Rosenthal <alfred.k.rosenthal@gmail.com>

--- e2e/test/bcsc/a11y.spec.ts
+++ e2e/test/bcsc/a11y.spec.ts
@@ -1,3 +1,5 @@
+// organize-imports-ignore — import order defines test run order
+
 import './a11y/account-setup.a11y.spec.js'
 import './a11y/setup-types-step.spec.js'
-import './onboarding/app-launch.spec.js'
+import './a11y/new-setup.a11y.spec.js'
