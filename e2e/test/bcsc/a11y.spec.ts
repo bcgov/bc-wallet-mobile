d0469dd3185213627fdd25cc52409f9f608a92fe
Alfred Rosenthal <alfred.k.rosenthal@gmail.com>
2026-03-31 16:09:05 -0700

    chore: cleaning up old code
    Signed-off-by: Alfred Rosenthal <alfred.k.rosenthal@gmail.com>

--- e2e/test/bcsc/a11y.spec.ts
+++
@@ -1,5 +0,0 @@
-// organize-imports-ignore — import order defines test run order
-
-import './a11y/account-setup.a11y.spec.js'
-import './a11y/setup-types-step.spec.js'
-import './a11y/new-setup.a11y.spec.js'
