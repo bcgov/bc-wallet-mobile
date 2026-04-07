171a110deec0eada347bdf7b6d6dadd86cf19ec9
Kjartan Einarsson <kjartanreinarsson@gmail.com>
2026-04-07 11:22:10 -0700

    feat: android biometrics
    Signed-off-by: Kjartan Einarsson <kjartanreinarsson@gmail.com>

--- e2e/src/helpers/sauce.ts
+++ e2e/src/helpers/sauce.ts
@@ -13,3 +13,23 @@ export async function annotate(message: string) {
     await browser.execute(`sauce:context=${message}`)
   }
 }
+
+/**
+ * Derive a human-readable job name from the spec file path.
+ * e.g. "full-regression/biometrics.spec.ts" -> "Full Regression / Biometrics"
+ */
+export function jobNameFromSpec(specPath: string): string {
+  const match = specPath.match(/test\/[^/]+\/(.+)\.spec\.\w+$/)
+  if (!match)
+    return (
+      specPath
+        .split('/')
+        .pop()
+        ?.replace(/\.spec\.\w+$/, '') ?? 'E2E Test'
+    )
+
+  return match[1]
+    .split('/')
+    .map((segment) => segment.replace(/-/g, ' ').replace(/\b\w/g, (c) => c.toUpperCase()))
+    .join(' / ')
+}
