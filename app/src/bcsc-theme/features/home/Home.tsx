a969359b31aa8df98b9d2841ecfcbd3d77c9501e
Mac Deluca <Mac.Deluca@quartech.com>
2026-04-23 11:08:47 -0700

    fix: keeping original home component for ref
    Signed-off-by: Mac Deluca <Mac.Deluca@quartech.com>

--- app/src/bcsc-theme/features/home/Home.tsx
+++ app/src/bcsc-theme/features/home/Home.tsx
@@ -1,17 +1,104 @@
+import { BCSCBanner } from '@/bcsc-theme/components/AppBanner'
+import { CardButton } from '@/bcsc-theme/components/CardButton'
+import { GENERIC_CARD_SIZE_SMALL } from '@/bcsc-theme/components/GenericCardImage'
+import { NotificationBannerContainer } from '@/bcsc-theme/components/NotificationBannerContainer'
 import TabScreenWrapper from '@/bcsc-theme/components/TabScreenWrapper'
+import { useAccount } from '@/bcsc-theme/contexts/BCSCAccountContext'
+import { LoadingScreen } from '@/bcsc-theme/contexts/BCSCLoadingContext'
+import { useBCSCApiClient } from '@/bcsc-theme/hooks/useBCSCApiClient'
 import { BCSCScreens, BCSCTabStackParams } from '@/bcsc-theme/types/navigators'
+import { BCState } from '@/store'
+import { testIdWithKey, useStore, useTheme } from '@bifold/core'
 import { StackScreenProps } from '@react-navigation/stack'
-import React from 'react'
+import { a11yLabel } from '@utils/accessibility'
+import React, { useCallback } from 'react'
+import { useTranslation } from 'react-i18next'
+import { StyleSheet, View } from 'react-native'
+import SectionButton from '../../components/SectionButton'
+import HomeHeader from './components/HomeHeader'
+import SavedServices from './components/SavedServices'
 
 type HomeProps = StackScreenProps<BCSCTabStackParams, BCSCScreens.Home>
 
 /**
- * Home screen for 4.1+ - currently just a placeholder
- *
- * @returns JSX.Element
+ * Home screen for >= V4.1.x
+ * @returns React element
  */
 const Home: React.FC<HomeProps> = () => {
   return <TabScreenWrapper></TabScreenWrapper>
+}
+
+/**
+ * FIXME (V4.1): This screen will be needed somewhere in the release, uncertain where it will be used. Keeping as reference.
+ */
+export const HomeV4_0_x: React.FC<HomeProps> = ({ navigation }) => {
+  const { t } = useTranslation()
+  const { Spacing } = useTheme()
+  const apiClient = useBCSCApiClient()
+  const { account } = useAccount()
+  const [store] = useStore<BCState>()
+
+  const handleManageDevices = useCallback(() => {
+    navigation.getParent()?.navigate(BCSCScreens.MainWebView, {
+      url: apiClient.endpoints.accountDevices,
+      title: t('BCSC.Screens.ManageDevices'),
+    })
+  }, [apiClient.endpoints.accountDevices, navigation, t])
+
+  const styles = StyleSheet.create({
+    buttonsContainer: {
+      padding: Spacing.md,
+    },
+  })
+
+  const handleWhereToUsePress = () => {
+    navigation.navigate(BCSCScreens.Services)
+  }
+
+  const handlePairingCodePress = () => {
+    navigation.getParent()?.navigate(BCSCScreens.ManualPairingCode)
+  }
+
+  const filteredBannerMessages = store.bcsc.bannerMessages.filter((b) => b.id === BCSCBanner.ACCOUNT_EXPIRING_SOON)
+
+  if (!account) {
+    return <LoadingScreen />
+  }
+
+  return (
+    <>
+      <NotificationBannerContainer onManageDevices={handleManageDevices} />
+      <TabScreenWrapper>
+        <HomeHeader name={account.fullname_formatted} cardSize={GENERIC_CARD_SIZE_SMALL} />
+        <View style={styles.buttonsContainer}>
+          <SectionButton
+            title={t('BCSC.Home.WhereToUseTitle')}
+            accessibilityLabel={a11yLabel(t('BCSC.Home.WhereToUseAccessibilityLabel'))}
+            description={t('BCSC.Home.WhereToUseDescription')}
+            style={{ marginBottom: Spacing.md }}
+            onPress={handleWhereToUsePress}
+            testID={testIdWithKey('WhereToUse')}
+          />
+          <SectionButton
+            title={t('BCSC.Home.LogInFromComputerTitle')}
+            accessibilityLabel={a11yLabel(t('BCSC.Home.LogInFromComputerAccessibilityLabel'))}
+            description={t('BCSC.Home.LogInFromComputerDescription')}
+            onPress={handlePairingCodePress}
+            testID={testIdWithKey('LogInFromComputer')}
+          />
+          {filteredBannerMessages.length > 0 && (
+            <View style={{ marginTop: Spacing.lg }}>
+              <CardButton
+                title={t('BCSC.AccountRenewal.WarningRenewButton')}
+                onPress={() => navigation.getParent()?.navigate(BCSCScreens.AccountRenewalInformation)}
+              />
+            </View>
+          )}
+        </View>
+        <SavedServices />
+      </TabScreenWrapper>
+    </>
+  )
 }
 
 export default Home
