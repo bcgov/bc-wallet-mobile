8c8d49d76e745f53f45cf3bfe055541d04238a47
Alfred Rosenthal <alfred.k.rosenthal@gmail.com>
2026-04-28 21:25:41 -0700

    feat: removed nickname screens and from navigation
    Signed-off-by: Alfred Rosenthal <alfred.k.rosenthal@gmail.com>

--- app/src/bcsc-theme/types/navigators.ts
+++ app/src/bcsc-theme/types/navigators.ts
@@ -89,7 +89,6 @@ export enum BCSCScreens {
   TransferAccountQRInformation = 'QR Get Overview',
   TransferAccountSuccess = 'QR Code Scan Complete',
   ServiceLogin = 'Login Request',
-  NicknameAccount = 'Choose Account Nickname',
   EditNickname = 'Change Account Nickname',
   OnboardingAccountSetup = 'Start Setup',
   OnboardingSetupTypes = 'Setup Options',
@@ -198,7 +197,6 @@ export type BCSCVerifyStackParams = {
   [BCSCScreens.LiveCall]: undefined
   [BCSCScreens.VerifyNotComplete]: undefined
   [BCSCScreens.ResidentialAddress]: undefined
-  [BCSCScreens.NicknameAccount]: undefined
   [BCSCScreens.VerifySettings]: undefined
   [BCSCScreens.VerifyPrivacyPolicy]: undefined
   [BCSCScreens.VerifyContactUs]: undefined
