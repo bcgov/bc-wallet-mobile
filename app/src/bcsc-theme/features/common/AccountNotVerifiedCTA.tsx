91ca5bc3e02826e003bd774b2dd6733a73ec72c5
Alfred Rosenthal <alfred.k.rosenthal@gmail.com>
2026-05-01 15:15:30 -0700

    feat: added svg and finished cta screen
    Signed-off-by: Alfred Rosenthal <alfred.k.rosenthal@gmail.com>

--- app/src/bcsc-theme/features/common/AccountNotVerifiedCTA.tsx
+++ app/src/bcsc-theme/features/common/AccountNotVerifiedCTA.tsx
@@ -1,3 +1,4 @@
+import Verified from '@assets/img/verified.svg'
 import { BCSCMainStackParams, BCSCScreens } from '@bcsc-theme/types/navigators'
 import { Button, ButtonType, ScreenWrapper, ThemedText, useTheme } from '@bifold/core'
 import { useNavigation } from '@react-navigation/native'
@@ -22,19 +23,22 @@ const AccountNotVerifiedCTA: React.FC = () => {
 
   return (
     <ScreenWrapper controls={controls}>
-      <ThemedText variant="headingThree" style={{ marginBottom: Spacing.md }}>
-        {t('BCSC.AccountNotVerified.Title')}
-      </ThemedText>
-      <ThemedText style={{ marginBottom: Spacing.lg }}>{t('BCSC.AccountNotVerified.Message')}</ThemedText>
-      <ThemedText style={{ marginTop: Spacing.md, fontSize: 24, fontWeight: 'bold' }}>
-        {t('BCSC.AccountNotVerified.Details.Title')}
-      </ThemedText>
-      {['Step1', 'Step2', 'Step3', 'Step4'].map((step) => (
-        <View key={step} style={{ flexDirection: 'row', marginTop: Spacing.xs }}>
-          <ThemedText>{'•  '}</ThemedText>
-          <ThemedText style={{ flex: 1 }}>{t(`BCSC.AccountNotVerified.Details.${step}`)}</ThemedText>
-        </View>
-      ))}
+      <View style={{ marginHorizontal: Spacing.md }}>
+        <Verified height={100} style={{ alignSelf: 'center', marginBottom: Spacing.md }} />
+        <ThemedText variant="headingThree" style={{ marginBottom: Spacing.md, alignSelf: 'center' }}>
+          {t('BCSC.AccountNotVerified.Title')}
+        </ThemedText>
+        <ThemedText style={{ marginBottom: Spacing.lg }}>{t('BCSC.AccountNotVerified.Message')}</ThemedText>
+        <ThemedText style={{ marginTop: Spacing.md, fontSize: 24, fontWeight: 'bold' }}>
+          {t('BCSC.AccountNotVerified.Details.Title')}
+        </ThemedText>
+        {['Step1', 'Step2', 'Step3', 'Step4'].map((step) => (
+          <View key={step} style={{ flexDirection: 'row', marginTop: Spacing.xs }}>
+            <ThemedText>{'•  '}</ThemedText>
+            <ThemedText style={{ flex: 1 }}>{t(`BCSC.AccountNotVerified.Details.${step}`)}</ThemedText>
+          </View>
+        ))}
+      </View>
     </ScreenWrapper>
   )
 }
