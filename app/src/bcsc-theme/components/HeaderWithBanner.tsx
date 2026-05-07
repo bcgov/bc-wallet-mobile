e13c81b52321eb65307bf7d8c86347685efebe55
Bryce McMath <32586431+bryce-mcmath@users.noreply.github.com>
2026-05-07 07:11:18 -0700

    feat: new services tab and animated tab bar (#3810)
    Signed-off-by: Bryce McMath <bryce.j.mcmath@gmail.com>

--- app/src/bcsc-theme/components/HeaderWithBanner.tsx
+++ app/src/bcsc-theme/components/HeaderWithBanner.tsx
@@ -1,5 +1,7 @@
 import { SHADOW_CASTER_HEIGHT, SHADOW_COLOR, SHADOW_OFFSET_DOWN, SHADOW_OPACITY, SHADOW_RADIUS } from '@/constants'
 import { useTheme } from '@bifold/core'
+import { BottomTabHeaderProps } from '@react-navigation/bottom-tabs'
+import { Header as ElementsHeader, getHeaderTitle } from '@react-navigation/elements'
 import { Header, StackHeaderProps } from '@react-navigation/stack'
 import React from 'react'
 import { View } from 'react-native'
@@ -71,5 +73,37 @@ export const createHeaderWithoutBanner = (props: StackHeaderProps) => (
     <Header {...props} />
   </View>
 )
+
+export const createTabHeaderWithoutBanner = ({ route, options, layout }: BottomTabHeaderProps) => (
+  <TabHeaderWithoutBanner route={route} options={options} layout={layout} />
+)
+
+const TabHeaderWithoutBanner = ({
+  route,
+  options,
+  layout,
+}: Pick<BottomTabHeaderProps, 'route' | 'options' | 'layout'>) => {
+  const { ColorPalette } = useTheme()
+  return (
+    <View>
+      <ElementsHeader {...options} layout={layout} title={getHeaderTitle(options, route.name)} />
+      <DropShadow
+        style={{
+          shadowColor: SHADOW_COLOR,
+          shadowOffset: SHADOW_OFFSET_DOWN,
+          shadowOpacity: SHADOW_OPACITY,
+          shadowRadius: SHADOW_RADIUS,
+        }}
+      >
+        <View
+          style={{
+            height: SHADOW_CASTER_HEIGHT,
+            backgroundColor: ColorPalette.brand.primaryBackground,
+          }}
+        />
+      </DropShadow>
+    </View>
+  )
+}
 
 export default HeaderWithBanner
