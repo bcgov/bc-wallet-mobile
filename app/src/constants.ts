84841bb3a7e5ce16a1ea968cdc6f51b6e0e3721b
Al Rosenthal <alfred.k.rosenthal@gmail.com>
2026-05-13 12:42:00 -0700

    feat: added email veriy help button (#3858)
    Signed-off-by: Alfred Rosenthal <alfred.k.rosenthal@gmail.com>

--- app/src/constants.ts
+++ app/src/constants.ts
@@ -75,6 +75,7 @@ export enum HelpCentreUrl {
   FORGOT_PIN = 'https://id.gov.bc.ca/static/help/secure_app.html?fromapp=1#section-forgotpin',
   INFO_SHARED = 'https://id.gov.bc.ca/static/help/info_shared.html?fromapp=1',
   COMPUTER_LOGIN = 'https://id.gov.bc.ca/static/help/login_fromcomputer.html?fromapp=1',
+  VERIFY_EMAIL = 'https://id.gov.bc.ca/static/help/email.html?fromapp=1',
 }
 
 export const formStringLengths = {
