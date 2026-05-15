fc4302efc5be98cb3fd8feab604ccf40fb442879
Kjartan <35311998+KjartanE@users.noreply.github.com>
2026-05-15 12:24:13 -0700

    feat: restyle additional id verification screens (#3864)
    Signed-off-by: Kjartan Einarsson <kjartanreinarsson@gmail.com>
    Signed-off-by: Kjartan Einarsson <35311998+KjartanE@users.noreply.github.com>
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>
    Co-authored-by: Jason C. Leach <jason.leach@fullboar.ca>
    Signed-off-by: Kjartan Einarsson <kjartanreinarsson@gmail.com>

--- app/src/bcsc-theme/features/verify/ResidentialAddressScreen.tsx
+++ app/src/bcsc-theme/features/verify/ResidentialAddressScreen.tsx
@@ -1,3 +1,4 @@
+import { ControlContainer } from '@/bcsc-theme/components/ControlContainer'
 import { DropdownWithValidation } from '@/bcsc-theme/components/DropdownWithValidation'
 import { InputWithValidation } from '@/bcsc-theme/components/InputWithValidation'
 import { BCSCScreens, BCSCVerifyStackParams } from '@/bcsc-theme/types/navigators'
@@ -13,7 +14,6 @@ import {
 } from '@bifold/core'
 import { StackScreenProps } from '@react-navigation/stack'
 import { useTranslation } from 'react-i18next'
-import { View } from 'react-native'
 import useResidentialAddressModel from './_models/useResidentialAddressModel'
 
 type ResidentialAddressScreenProps = StackScreenProps<BCSCVerifyStackParams, BCSCScreens.ResidentialAddress>
@@ -25,61 +25,88 @@ type ResidentialAddressScreenProps = StackScreenProps<BCSCVerifyStackParams, BCS
  */
 export const ResidentialAddressScreen = ({ navigation }: ResidentialAddressScreenProps) => {
   const { t } = useTranslation()
-  const { Spacing } = useTheme()
+  const { ColorPalette, Spacing } = useTheme()
   const { ButtonLoading } = useAnimatedComponents()
 
   const { formState, formErrors, isSubmitting, handleChange, handleSubmit } = useResidentialAddressModel({ navigation })
 
-  return (
-    <ScreenWrapper keyboardActive={true} scrollViewContainerStyle={{ gap: Spacing.lg }}>
-      <ThemedText variant={'headingThree'}>{t('BCSC.Address.Heading')}</ThemedText>
+  const labelProps = { color: ColorPalette.brand.primary }
 
+  const controls = (
+    <ControlContainer>
+      <Button
+        title={t('Global.Continue')}
+        accessibilityLabel={t('Global.Continue')}
+        testID={testIdWithKey('ResidentialAddressContinue')}
+        buttonType={ButtonType.Primary}
+        onPress={handleSubmit}
+        disabled={isSubmitting}
+      >
+        {isSubmitting && <ButtonLoading />}
+      </Button>
+    </ControlContainer>
+  )
+
+  return (
+    <ScreenWrapper
+      keyboardActive
+      padded={false}
+      controls={controls}
+      scrollViewContainerStyle={{
+        flexGrow: 1,
+        gap: Spacing.sm,
+        padding: Spacing.lg,
+      }}
+    >
+      <ThemedText variant={'headingThree'}>{t('BCSC.Address.Heading')}</ThemedText>
       <ThemedText>{t('BCSC.Address.Paragraph')}</ThemedText>
 
       <InputWithValidation
         id={'streetAddress1'}
         label={t('BCSC.Address.StreetAddressLabel')}
+        labelProps={labelProps}
         value={formState.streetAddress}
         onChange={(value) => handleChange('streetAddress', value)}
         error={formErrors.streetAddress}
-        subtext={t('BCSC.Address.StreetAddressSubtext')}
         textInputProps={{ autoCorrect: false, autoComplete: 'address-line1', textContentType: 'streetAddressLine1' }}
       />
 
       <InputWithValidation
         id={'streetAddress2'}
         label={t('BCSC.Address.StreetAddress2Label')}
+        labelProps={labelProps}
         value={formState.streetAddress2}
         onChange={(value) => handleChange('streetAddress2', value)}
         error={formErrors.streetAddress2}
-        subtext={t('BCSC.Address.StreetAddress2Subtext')}
         textInputProps={{ autoCorrect: false, autoComplete: 'address-line2', textContentType: 'streetAddressLine2' }}
       />
 
       <InputWithValidation
         id={'city'}
         label={t('BCSC.Address.CityLabel')}
+        labelProps={labelProps}
         value={formState.city}
         onChange={(value) => handleChange('city', value)}
         error={formErrors.city}
-        subtext={t('BCSC.Address.CitySubtext')}
         textInputProps={{ autoCorrect: false, autoComplete: 'postal-address-locality', textContentType: 'addressCity' }}
       />
 
       <DropdownWithValidation
         id={'province'}
         label={t('BCSC.Address.ProvinceLabel')}
+        labelProps={labelProps}
         value={formState.province}
         options={PROVINCE_OPTIONS}
         onChange={(value) => handleChange('province', value)}
         error={formErrors.province}
-        placeholder={t('BCSC.Address.ProvinceSubtext')}
+        placeholder={t('BCSC.Address.ProvincePlaceholder')}
         subtext={t('BCSC.Address.ProvinceSubtext')}
       />
 
       <InputWithValidation
         id={'postalCode'}
         label={t('BCSC.Address.PostalCodeLabel')}
+        labelProps={labelProps}
         value={formState.postalCode}
         onChange={(value) => handleChange('postalCode', value)}
         error={formErrors.postalCode}
@@ -91,21 +118,6 @@ export const ResidentialAddressScreen = ({ navigation }: ResidentialAddressScree
           textContentType: 'postalCode',
         }}
       />
-
-      <View style={{ marginTop: 48, width: '100%' }}>
-        <View style={{ marginBottom: 20 }}>
-          <Button
-            title={t('Global.Continue')}
-            accessibilityLabel={t('Global.Continue')}
-            testID={testIdWithKey('ResidentialAddressContinue')}
-            buttonType={ButtonType.Primary}
-            onPress={handleSubmit}
-            disabled={isSubmitting}
-          >
-            {isSubmitting && <ButtonLoading />}
-          </Button>
-        </View>
-      </View>
     </ScreenWrapper>
   )
 }
