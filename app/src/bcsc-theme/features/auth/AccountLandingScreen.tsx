ec53b281442c5733cffa185f25fee078b0df6f79
Bryce McMath <32586431+bryce-mcmath@users.noreply.github.com>
2026-05-20 13:58:11 -0700

    feat: account landing restyle (#3886)
    Signed-off-by: Bryce McMath <bryce.j.mcmath@gmail.com>
    Signed-off-by: Kjartan Einarsson <kjartanreinarsson@gmail.com>
    Signed-off-by: Kjartan Einarsson <35311998+KjartanE@users.noreply.github.com>

---
+++ app/src/bcsc-theme/features/auth/AccountLandingScreen.tsx
@@ -0,0 +1,72 @@
+import BCSCLogo from '@/assets/img/BCSCLogo.svg'
+import { ControlContainer } from '@/bcsc-theme/components/ControlContainer'
+import { NotificationBannerContainer } from '@/bcsc-theme/components/NotificationBannerContainer'
+import { useAuthentication } from '@/bcsc-theme/hooks/useAuthentication'
+import { BCSCAuthStackParams, BCSCScreens } from '@/bcsc-theme/types/navigators'
+import { BCDispatchAction, BCState } from '@/store'
+import { Button, ButtonType, ScreenWrapper, testIdWithKey, ThemedText, useStore, useTheme } from '@bifold/core'
+import { StackNavigationProp } from '@react-navigation/stack'
+import React, { useCallback } from 'react'
+import { useTranslation } from 'react-i18next'
+import { StyleSheet, View } from 'react-native'
+
+interface AccountLandingScreenProps {
+  navigation: StackNavigationProp<BCSCAuthStackParams, BCSCScreens.AccountLanding>
+}
+
+const AccountLandingScreen = ({ navigation }: AccountLandingScreenProps) => {
+  const [store, dispatch] = useStore<BCState>()
+  const { t } = useTranslation()
+  const { Spacing } = useTheme()
+  const authentication = useAuthentication(navigation)
+
+  const styles = StyleSheet.create({
+    contentContainer: {
+      flexGrow: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: Spacing.lg,
+      gap: Spacing.lg,
+    },
+    image: {
+      marginTop: Spacing.xxl,
+      alignItems: 'center',
+    },
+  })
+
+  const handleAccountSelect = useCallback(async () => {
+    const nickname = store.bcsc.nicknames?.[0]
+    if (nickname) {
+      dispatch({ type: BCDispatchAction.SELECT_ACCOUNT, payload: [nickname] })
+    }
+    await authentication.unlockApp()
+  }, [authentication, dispatch, store.bcsc.nicknames])
+
+  const controls = (
+    <ControlContainer>
+      <Button
+        buttonType={ButtonType.Primary}
+        accessibilityLabel={t('BCSC.AccountLanding.Unlock')}
+        title={t('BCSC.AccountLanding.Unlock')}
+        onPress={handleAccountSelect}
+        testID={testIdWithKey('Unlock')}
+      />
+    </ControlContainer>
+  )
+
+  return (
+    <>
+      <NotificationBannerContainer onManageDevices={() => {}} />
+      <ScreenWrapper padded={false} scrollable scrollViewContainerStyle={styles.contentContainer} controls={controls}>
+        <View style={styles.image}>
+          <BCSCLogo width={120} height={120} />
+        </View>
+        <ThemedText variant={'headingFour'} style={{ textAlign: 'center' }}>
+          {t('BCSC.AccountSetup.Title')}
+        </ThemedText>
+      </ScreenWrapper>
+    </>
+  )
+}
+
+export default AccountLandingScreen
