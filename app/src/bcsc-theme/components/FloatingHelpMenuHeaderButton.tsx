fba6ab1cbb8410b79fa3da025affeb07ff957ff4
Kjartan Einarsson <kjartanreinarsson@gmail.com>
2026-07-09 11:45:39 -0700

    fix: remove kebab icon from verification stack. back to help icon
    Signed-off-by: Kjartan Einarsson <kjartanreinarsson@gmail.com>
    Signed-off-by: Kjartan Einarsson <35311998+KjartanE@users.noreply.github.com>

--- app/src/bcsc-theme/components/FloatingHelpMenuHeaderButton.tsx
+++ app/src/bcsc-theme/components/FloatingHelpMenuHeaderButton.tsx
@@ -15,7 +15,6 @@ import { ReportProblemModal } from './ReportProblemModal'
 // whose menu also offers navigation actions ("Back to home", "Restart") and so reads better as a
 // vertical-ellipsis "more options" affordance.
 const DEFAULT_HELP_ICON = 'help-circle-outline'
-const VERIFY_HELP_ICON = 'dots-vertical'
 
 type FloatingHelpMenuButtonProps = {
   // ListButton rows; falsy children are filtered out by ListButtonGroup so rows can be conditional
@@ -218,7 +217,7 @@ export const createVerifyHelpMenuButton = ({ showRestartVerification = false }: 
 
     return (
       <>
-        <FloatingHelpMenuButton ref={floatingHelpMenuRef} icon={VERIFY_HELP_ICON}>
+        <FloatingHelpMenuButton ref={floatingHelpMenuRef} icon={DEFAULT_HELP_ICON}>
           <ReportProblemListButton onPress={reportProblem.open} />
           <BackToHomeListButton onClose={closeMenu} />
           {showRestartVerification && <RestartVerificationListButton onClose={closeMenu} />}
