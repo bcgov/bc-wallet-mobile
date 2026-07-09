544b1e5046a2325afbc8a82705c47bd93521faea
Mac Deluca <Mac.Deluca@quartech.com>
2026-07-09 14:24:49 -0700

    fix: added formatAccountName
    Signed-off-by: Mac Deluca <Mac.Deluca@quartech.com>

--- app/src/bcsc-theme/utils/account-utils.ts
+++ app/src/bcsc-theme/utils/account-utils.ts
@@ -12,3 +12,36 @@ export const getNicknameValidationErrorKey = (_state: BCState, nickname: string)
 
   return null
 }
+
+/**
+ * Formats an account name and supports mononyms
+ *
+ * @example
+ *  const account = { firstName: 'Steve', middleNames: 'John', lastName: 'Brule' }
+ *  const formattedName = formatAccountName(account)
+ *  console.log(formattedName) // Output: "Brule, Steve John"
+ *
+ * @param account - The account object containing firstName, middleNames, and lastName
+ * @returns The formatted account name as a string
+ */
+export const formatAccountName = (account: { firstName?: string; middleNames?: string; lastName?: string }): string => {
+  const formattedNameParts: string[] = []
+
+  if (account.lastName) {
+    formattedNameParts.push(account.lastName.trim())
+  }
+
+  if (account.firstName) {
+    formattedNameParts.push(account.firstName.trim())
+  }
+
+  if (account.middleNames) {
+    formattedNameParts.push(account.middleNames.trim())
+  }
+
+  if (formattedNameParts.length > 1 && account.lastName) {
+    formattedNameParts[0] = formattedNameParts[0] += ','
+  }
+
+  return formattedNameParts.join(' ')
+}
