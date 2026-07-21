ea68ee84f1545fcbfdf3075ac80a56709d86a8c6
Bryce McMath <bryce.j.mcmath@gmail.com>
2026-07-20 18:04:44 -0700

    wow: embarrassing
    Signed-off-by: Bryce McMath <bryce.j.mcmath@gmail.com>

--- app/container-imp.ts
+++ app/container-imp.ts
@@ -212,7 +212,7 @@ export class AppContainer implements Container {
           ],
         },
       ],
-      enableTours: Config.BUILD_TARGET === Mode.BCWallet ? true : false,
+      enableTours: Config.BUILD_TARGET === Mode.BCWallet,
       enableChat: true,
       enableReuseConnections: true,
       enableHiddenDevModeTrigger: true,
