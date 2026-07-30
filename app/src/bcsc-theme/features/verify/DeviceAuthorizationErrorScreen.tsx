1cd29c468a5486013a02d161286a5b08f271ddc0
Alfred Rosenthal <alfred.k.rosenthal@gmail.com>
2026-07-30 15:41:01 -0700

    feat: added icon to button
    Signed-off-by: Alfred Rosenthal <alfred.k.rosenthal@gmail.com>

--- app/src/bcsc-theme/features/verify/DeviceAuthorizationErrorScreen.tsx
+++ app/src/bcsc-theme/features/verify/DeviceAuthorizationErrorScreen.tsx
@@ -1,15 +1,17 @@
 import { ControlContainer } from '@/bcsc-theme/components/ControlContainer'
 import { openLink } from '@/utils/links'
-import { Button, ButtonType, ScreenWrapper, testIdWithKey, ThemedText, useTheme } from '@bifold/core'
+import { Button, ButtonType, ColorPalette, ScreenWrapper, testIdWithKey, ThemedText, useTheme } from '@bifold/core'
 import { RouteProp, useRoute } from '@react-navigation/native'
 import { useTranslation } from 'react-i18next'
+import { View } from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 import { BCSCScreens, BCSCVerifyStackParams } from '../../types/navigators'
 import { DEVICE_AUTHORIZATION_ERROR_CONFIG } from './deviceAuthorizationError'
 
 export { DeviceAuthorizationError } from './deviceAuthorizationError'
 
 const DeviceAuthorizationErrorScreen = () => {
-  const { Spacing } = useTheme()
+  const { Spacing, Buttons } = useTheme()
   const { t } = useTranslation()
   const { params } = useRoute<RouteProp<BCSCVerifyStackParams, BCSCScreens.DeviceAuthorizationError>>()
 
@@ -18,18 +20,27 @@ const DeviceAuthorizationErrorScreen = () => {
   const controls = (
     <ControlContainer>
       <Button
-        title={t(config.buttonTextKey)}
+        title=""
         accessibilityLabel={t(config.buttonTextKey)}
         accessibilityHint={t('Global.A11y.OpensInBrowser')}
         testID={testIdWithKey('DeviceAuthorizationErrorLink')}
         buttonType={ButtonType.Primary}
         onPress={() => openLink(config.buttonUrl)}
-      />
+      >
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <ThemedText style={Buttons.primaryText}>{t(config.buttonTextKey)}</ThemedText>
+          <Icon name="open-in-new" size={20} color={ColorPalette.brand.headerText} style={{ marginLeft: Spacing.sm }} />
+        </View>
+      </Button>
     </ControlContainer>
   )
 
   return (
-    <ScreenWrapper padded={false} controls={controls} scrollViewContainerStyle={{ gap: Spacing.md, padding: Spacing.lg }}>
+    <ScreenWrapper
+      padded={false}
+      controls={config.buttonVisible ? controls : null}
+      scrollViewContainerStyle={{ gap: Spacing.md, padding: Spacing.lg }}
+    >
       <ThemedText variant={'headingThree'}>{t(config.headingKey)}</ThemedText>
       <ThemedText>{t(config.descriptionKey)}</ThemedText>
     </ScreenWrapper>
