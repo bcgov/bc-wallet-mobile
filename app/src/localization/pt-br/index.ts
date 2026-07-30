f850681440fc9b0aae90b12be8ffb61a6c76ddef
Bryce McMath <bryce.j.mcmath@gmail.com>
2026-07-30 10:47:36 -0700

    chore: update text content
    Signed-off-by: Bryce McMath <bryce.j.mcmath@gmail.com>

--- app/src/localization/pt-br/index.ts
+++ app/src/localization/pt-br/index.ts
@@ -125,13 +125,13 @@ const translation = {
       "ButtonTitle": "Finish up (PT-BR)"
     },
     "AccountExpired": {
-      "Title": "Your BC Services card account has expired (PT-BR)",
+      "Title": "Your BC Services Card account has expired (PT-BR)",
       "Description": "Please reverify yourself to get access to your services (PT-BR)",
       "ButtonTitle": "Start renewal (PT-BR)"
     },
     "AccountRenewal": {
       "Title": "Time for renewal (PT-BR)",
-      "Description": "Renew your BC Services card account before it expires on {{expiryDate}} (PT-BR)",
+      "Description": "Renew your BC Services Card account before it expires on {{expiryDate}} (PT-BR)",
       "ButtonTitle": "Start renewal (PT-BR)"
     },
     "BasicMessage": {
@@ -259,7 +259,7 @@ const translation = {
     "Components": {
       "CloseDropdown": "Close dropdown menu (PT-BR)",
     },
-    "Title": "BC Services card (PT-BR)",
+    "Title": "BC Services Card (PT-BR)",
     "LocaleStringFormat": "pt-BR",
     "Screens": {
       "Settings": "Menu (PT-BR)",
@@ -367,7 +367,7 @@ const translation = {
       "AppStartup": "Getting your BCSC App ready... (PT-BR)"
     },
     "AccountLanding": {
-      "Title": "BC Services card (PT-BR)",
+      "Title": "BC Services Card (PT-BR)",
       "Description": "A secure way to prove\nwho you are online (PT-BR)",
       "Unlock": "Unlock (PT-BR)",
     },
@@ -430,7 +430,7 @@ const translation = {
       "HelpHeader": "Help (PT-BR)",
       "MoreInfoHeader": "More Information (PT-BR)",
       "Help": "Help (PT-BR)",
-      "HelpUsingApp": "Help using BCSC app (PT-BR)",
+      "HelpUsingApp": "Help using BC Services Card app(PT-BR)",
       "SignOut": "Sign out (PT-BR)",
       "Biometrics": "Biometrics (PT-BR)",
       "EditNickname": "Edit Nickname (PT-BR)",
@@ -637,7 +637,7 @@ const translation = {
     },
     "ReverifyAccount": {
       "RenewalTitle": "Renew your account (PT-BR)",
-      "ExpiredTitle": "Your BC Services card account has expired (PT-BR)",
+      "ExpiredTitle": "Your BC Services Card account has expired (PT-BR)",
       "Body": "Services require you to re-verify your account. You will need to complete this step in order to access your services online. (PT-BR)",
       "BulletHeader": "You will need to: (PT-BR)",
       "BulletOne": "Verify your email (PT-BR)",
@@ -843,7 +843,7 @@ const translation = {
     },
     "AccountSetup": {
       "Title": "Have you verified before? (PT-BR)",
-      "Description": "Do you already have the BCSC app set up on another nearby phone or tablet? (PT-BR)",
+      "Description": "Do you already have the BC Services Card appset up on another nearby phone or tablet? (PT-BR)",
       "AddAccount": "No, continue setup (PT-BR)",
       "TransferAccount": "Yes, connect this device (PT-BR)",
       "ContinueAs": "Continue as: (PT-BR)",
