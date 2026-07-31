9bc300cc4dcc11e4e2fb32e9219aaab5bf0ca4f6
Alfred Rosenthal <alfred.k.rosenthal@gmail.com>
2026-07-31 16:01:47 -0700

    feat: created new pending system check
    Signed-off-by: Alfred Rosenthal <alfred.k.rosenthal@gmail.com>

---
+++ app/src/services/system-checks/PendingVerificationRecoverySystemCheck.ts
@@ -0,0 +1,36 @@
+import { BCDispatchAction } from '@/store'
+import { SystemCheckStrategy, SystemCheckUtils } from './system-checks'
+
+/**
+ * Recovers a pending in-person or live-call verification (VerificationRequestSystemCheck handles send-video requests)
+ * if the app was closed before the app itself detected success (App was closed before "Complete" was pressed)
+ */
+export class PendingVerificationRecoverySystemCheck implements SystemCheckStrategy {
+  private readonly checkVerificationStatus: () => Promise<boolean>
+  private readonly utils: SystemCheckUtils
+
+  constructor(checkVerificationStatus: () => Promise<boolean>, utils: SystemCheckUtils) {
+    this.checkVerificationStatus = checkVerificationStatus
+    this.utils = utils
+  }
+
+  async runCheck(): Promise<boolean> {
+    return this.checkVerificationStatus()
+  }
+
+  onFail() {
+    // Not yet verified — noop
+  }
+
+  onSuccess() {
+    // Set request status to verified to display a CTA to "complete" the verification process
+    this.utils.dispatch({
+      type: BCDispatchAction.UPDATE_SECURE_VERIFICATION_REQUEST_STATUS,
+      payload: ['verified'],
+    })
+    this.utils.dispatch({
+      type: BCDispatchAction.UPDATE_SECURE_VERIFICATION_REQUEST_STATUS_MESSAGE,
+      payload: [undefined],
+    })
+  }
+}
