b14806a7c243195156a6e24d9af090823b2f0264
Jason C. Leach <jason.leach@fullboar.ca>
2026-08-18 16:40:21 -0700

    fix(bcsc): strip site chrome on the privacy webview, drop dead constants
    Append ?fromapp=1 to BC_LOGIN_PRIVACY_URL, matching every other in-app
    id.gov WebView URL. Also remove WHERE_TO_USE_URL and ACCOUNT_SERVICES_URL,
    which had zero consumers left in the repo.
    
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- app/src/constants.ts
+++ app/src/constants.ts
@@ -54,15 +54,13 @@ export const BCThemeNames = {
 export const BCSC_EMAIL_NOT_PROVIDED = 'Not provided'
 export const BCSC_APPLE_STORE_URL = 'https://apps.apple.com/us/app/id1234298467'
 export const BCSC_GOOGLE_PLAY_STORE_URL = 'https://play.google.com/store/apps/details?id=ca.bc.gov.id.servicescard'
-export const ACCOUNT_SERVICES_URL = 'https://id.gov.bc.ca/account/services'
 export const TERMS_OF_USE_URL = 'https://id.gov.bc.ca/static/termsOfUse.html'
 export const FEEDBACK_URL = 'https://id.gov.bc.ca/static/feedback.html'
 export const ACCESSIBILITY_URL =
   'https://www2.gov.bc.ca/gov/content/governments/government-id/bcservicescardapp#accessibility'
 // appending param fromapp=1 to certain id.gov urls automatically removes header and footer and such
-export const BC_LOGIN_PRIVACY_URL = 'https://id.gov.bc.ca/static/privacy.html'
+export const BC_LOGIN_PRIVACY_URL = 'https://id.gov.bc.ca/static/privacy.html?fromapp=1'
 export const SECURE_APP_LEARN_MORE_URL = 'https://id.gov.bc.ca/static/help/secure_app.html?fromapp=1'
-export const WHERE_TO_USE_URL = ACCOUNT_SERVICES_URL
 export const CONTACT_US_HELP_URL = 'https://id.gov.bc.ca/static/help/contact-us.html'
 export const GET_BCSC_CARD_URL =
   'https://www2.gov.bc.ca/gov/content/governments/government-id/bc-services-card/your-card/get-a-card'
