4e53cb3c48a8e0284effe4045f6b24682f52d85b
Alfred Rosenthal <alfred.k.rosenthal@gmail.com>
2026-09-04 14:34:39 -0700

    feat: refactored screen
    Signed-off-by: Alfred Rosenthal <alfred.k.rosenthal@gmail.com>

--- app/src/bcsc-theme/features/modal/ServiceOutage.tsx
+++ app/src/bcsc-theme/features/modal/ServiceOutage.tsx
@@ -1,14 +1,27 @@
-import { CardButton } from '@/bcsc-theme/components/CardButton'
+import { ControlContainer } from '@/bcsc-theme/components/ControlContainer'
 import usePreventGestureBack from '@/hooks/usePreventGestureBack'
-import { Button, ButtonType, testIdWithKey, ThemedText, useTheme } from '@bifold/core'
-import { ScrollView, StyleSheet, View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import {
+  Button,
+  ButtonType,
+  ScreenWrapper,
+  testIdWithKey,
+  ThemedText,
+  useAnimatedComponents,
+  useTheme,
+} from '@bifold/core'
+import { useState } from 'react'
+import { StyleSheet, View } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 import useServiceOutageViewModel from './useServiceOutageViewModel'
 
-export const ServiceOutage = (): React.ReactElement => {
-  const { headerText, contentText, learnMoreText, buttonText, isCheckDisabled, handleCheckAgain, handleLearnMore } =
-    useServiceOutageViewModel()
+export interface ServiceOutageProps {
+  inOnboarding?: boolean
+}
+
+export const ServiceOutage = ({ inOnboarding = false }: ServiceOutageProps): React.ReactElement => {
+  const { headerText, contentText, buttonText, isCheckDisabled, handleCheckAgain } = useServiceOutageViewModel()
+  const { ButtonLoading } = useAnimatedComponents()
+  const [loading, setLoading] = useState(false)
   const { Spacing, ColorPalette } = useTheme()
 
   usePreventGestureBack()
@@ -35,36 +48,60 @@ export const ServiceOutage = (): React.ReactElement => {
     },
   })
 
-  return (
-    <SafeAreaView style={styles.container} edges={['bottom', 'left', 'right']}>
-      <ScrollView contentContainerStyle={styles.scrollContainer}>
-        <Icon name="error-outline" size={200} color={ColorPalette.brand.icon} style={styles.icon} />
-        <View style={styles.textContainer}>
-          <ThemedText variant="headingThree">{headerText}</ThemedText>
-          {contentText.filter(Boolean).map((text) => (
-            <ThemedText key={text} style={styles.textContent}>
-              {text}
-            </ThemedText>
-          ))}
-          <CardButton
-            title={learnMoreText}
-            onPress={handleLearnMore}
-            endIcon="open-in-new"
-            testID={testIdWithKey('ServiceOutageHelpCentre')}
-          />
-        </View>
-      </ScrollView>
+  const handleControls = async () => {
+    setLoading(true)
+    await handleCheckAgain()
+    setLoading(false)
+  }
 
-      <View style={styles.buttonContainer}>
+  const controls = (
+    <ControlContainer>
+      <Button
+        title={buttonText}
+        buttonType={ButtonType.Primary}
+        onPress={handleControls}
+        disabled={isCheckDisabled}
+        accessibilityLabel={buttonText}
+        testID={testIdWithKey('ServiceOutageCheckAgain')}
+      >
+        {loading && <ButtonLoading />}
+      </Button>
+      {inOnboarding && (
         <Button
-          title={buttonText}
-          buttonType={ButtonType.Primary}
-          onPress={handleCheckAgain}
-          disabled={isCheckDisabled}
-          accessibilityLabel={buttonText}
-          testID={testIdWithKey('ServiceOutageCheckAgain')}
+          title={'Learn more'}
+          buttonType={ButtonType.Secondary}
+          onPress={() => {
+            // TODO: this needs to act like the onboarding screen that prompts verification or skip
+          }}
+          accessibilityLabel={'Learn more'}
+          testID={testIdWithKey('ServiceOutageLearnMore')}
         />
+      )}
+    </ControlContainer>
+  )
+
+  return (
+    <ScreenWrapper
+      keyboardActive
+      padded={false}
+      controls={controls}
+      scrollViewContainerStyle={{ gap: Spacing.md, padding: Spacing.lg }}
+    >
+      <Icon name="error-outline" size={100} color={ColorPalette.brand.icon} style={styles.icon} />
+      <View style={styles.textContainer}>
+        <ThemedText variant="headingThree">{headerText}</ThemedText>
+        {contentText.filter(Boolean).map((text) => (
+          <ThemedText key={text} style={styles.textContent}>
+            {text}
+          </ThemedText>
+        ))}
+
+        <ThemedText style={styles.textContent}>
+          {"In the meantime, check the service you're trying to access for other ways to log in."}
+        </ThemedText>
+
+        <ThemedText style={styles.textContent}>{'If you need help, contact us'}</ThemedText>
       </View>
-    </SafeAreaView>
+    </ScreenWrapper>
   )
 }
