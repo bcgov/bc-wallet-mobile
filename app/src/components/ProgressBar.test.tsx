170177a0b7257a4a93201729df9ab623f67ae9e2
Jason C. Leach <jason.leach@fullboar.ca>
2026-09-08 15:43:30 -0700

    test(progress): remove prop passthrough assertion
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- app/src/components/ProgressBar.test.tsx
+++ app/src/components/ProgressBar.test.tsx
@@ -1,7 +1,6 @@
 import { BasicAppContext } from '@mocks/helpers/app'
 import { render, waitFor } from '@testing-library/react-native'
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
 import ProgressBar from './ProgressBar'
 
 describe('ProgressBar Component', () => {
@@ -35,13 +34,5 @@ describe('ProgressBar Component', () => {
     await waitFor(() => {
       expect(tree).toMatchSnapshot()
     })
-  })
-
-  test('supports screen-specific colors', () => {
-    const tree = render(<ProgressBar progressPercent={0} trackColor="#FAF9F8" progressColor="#F8BA47" />)
-    const [track, fill] = tree.UNSAFE_getAllByType(View)
-
-    expect(StyleSheet.flatten(track.props.style)).toMatchObject({ height: 11, backgroundColor: '#FAF9F8' })
-    expect(StyleSheet.flatten(fill.props.style)).toMatchObject({ backgroundColor: '#F8BA47' })
   })
 })
