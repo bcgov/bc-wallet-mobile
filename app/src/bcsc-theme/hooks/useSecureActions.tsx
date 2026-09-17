304d37b0e11589c41c88a81a29e0d7fac990ceab
Alfred Rosenthal <alfred.k.rosenthal@gmail.com>
2026-09-17 14:18:52 -0700

    chore: ran format
    Signed-off-by: Alfred Rosenthal <alfred.k.rosenthal@gmail.com>

--- app/src/bcsc-theme/hooks/useSecureActions.tsx
+++ app/src/bcsc-theme/hooks/useSecureActions.tsx
@@ -1077,7 +1077,10 @@ export const useSecureActions = () => {
           userMetadata.name = {
             first: stripDisallowedNameCharacters(authRequest.firstName),
             last: stripDisallowedNameCharacters(authRequest.lastName),
-            middle: authRequest.middleNames !== undefined ? stripDisallowedNameCharacters(authRequest.middleNames) : undefined,
+            middle:
+              authRequest.middleNames !== undefined
+                ? stripDisallowedNameCharacters(authRequest.middleNames)
+                : undefined,
           }
         }
       }
